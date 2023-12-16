977f71c4ed89033f957a2c0651e299d94603c06c
Greg <greg@hiro.so>
2023-12-15 21:42:54 -0500

    factored out "weights"

--- node/model/maxent.js
+++ node/model/maxent.js
@@ -1,27 +1,7 @@
 const logger = require("../logger")
 const { Implication } = require("./predicate")
 const assert = require("../assert")
-
-function RandomWeight() {
-    return (Math.random() - Math.random()) / 5
-}
-
-function PositiveFeature(feature) {
-    return '++' + feature + '++'
-}
-
-function NegativeFeature(feature) {
-    return '--' + feature + '--'
-}
-
-async function InitializeWeights(redis, implication) {
-    assert.isType(implication, Implication)
-    const weight1 = RandomWeight()
-    const feature = implication.UniqueKey()
-    await redis.client.hSet('weights', PositiveFeature(feature), weight1);
-    const weight2 = RandomWeight()
-    await redis.client.hSet('weights', NegativeFeature(feature), weight2);
-}
+const {PositiveFeature, NegativeFeature, InitializeWeights, ReadWeights, SaveWeights} = require("./weights")
 
 async function GetPropositionProbability(searchString) {
     assert.isType(searchString, "string")
@@ -69,28 +49,6 @@ function ComputeProbability(weights, features) {
     const dot = DotProduct(weights, features);
     const probability = Sigmoid(dot);
     return probability;
-}
-
-async function ReadWeights(features) {
-    logger.noop({ features }, ReadWeights)
-    var r = {}
-    for (const feature of Object.keys(features)) {
-        const record = await WeightRecord.findOne({ feature })
-        logger.noop({ feature, record }, ReadWeights)
-        if (record) {
-            r[feature] = record.weight
-        }
-    }
-    logger.noop({ r }, ReadWeights)
-    return r
-}
-
-async function SaveWeights(weights) {
-    logger.noop({ weights }, SaveWeights)
-    for (const feature of Object.keys(weights)) {
-        const updated = await WeightRecord.findOneAndUpdate({ feature }, { weight: weights[feature] }, { new: true, runValidators: true });
-        logger.noop({ feature, updated }, SaveWeights)
-    }
 }
 
 function ComputeExpectedFeatures(probability, features) {
