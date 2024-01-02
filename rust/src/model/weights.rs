6a5b518edaed72c6775a48f32b0aea0edb9f61ae
Greg <greg@hiro.so>
2024-01-01 21:35:15 -0500

    added read_weights

--- rust/src/model/weights.rs
+++ rust/src/model/weights.rs
@@ -2,6 +2,7 @@ use redis::{Commands, Client};
 use std::error::Error;
 use crate::model::objects::{Domain, Entity, Proposition, Implication};
 use rand::Rng;
+use std::collections::HashMap;
 
 fn random_weight() -> f64 {
     let mut rng = rand::thread_rng();
@@ -32,4 +33,21 @@ pub fn initialize_weights(redis_client: &Client, implication: &Implication) -> R
         .map_err(|e| Box::new(e) as Box<dyn Error>)?;
 
     Ok(())
+}
+
+pub fn read_weights(redis_client: &Client, features: &[String]) -> Result<HashMap<String, f64>, Box<dyn Error>> {
+    let mut con = redis_client.get_connection()
+        .map_err(|e| Box::new(e) as Box<dyn Error>)?;
+
+    let mut weights = HashMap::new();
+    for feature in features {
+        let record: String = con.hget("weights", feature)
+            .map_err(|e| Box::new(e) as Box<dyn Error>)?;
+        
+        let weight = record.parse::<f64>()
+            .map_err(|e| Box::new(e) as Box<dyn Error>)?;
+        weights.insert(feature.clone(), weight);
+    }
+
+    Ok(weights)
 }
