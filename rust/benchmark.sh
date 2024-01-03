6d5c0c17acbac417d89c683473998bf3fc5b537c
Greg <greg@hiro.so>
2024-01-02 20:19:11 -0500

    add the benchmark file.. wondering if the scenario should be facgtored less explosively

---
+++ rust/benchmark.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# First, compile the program
+cargo build --release
+
+# Array of different entities_per_domain values
+entities_values=(10 50 100 150 200)
+
+# Loop through each value and run the compiled program
+for value in "${entities_values[@]}"
+do
+    echo "Running with entities_per_domain = $value"
+    time ./target/release/bayes-star --entities_per_domain=$value
+done
