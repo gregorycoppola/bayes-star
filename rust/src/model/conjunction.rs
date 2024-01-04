fda910fab7c93104dcc98c5228e9c420dc94c04f
Greg <greg@hiro.so>
2024-01-04 11:27:47 -0500

    implement conjunction probability using min.. has to be some kind of approximation

--- rust/src/model/conjunction.rs
+++ rust/src/model/conjunction.rs
@@ -6,5 +6,11 @@ pub fn get_conjunction_probability(
     storage: &mut Storage,
     conjunction: &Conjunction,
 ) -> Result<f64, Box<dyn Error>> {
-    todo!("implement me")
+    let mut min_prob = 1f64;
+    for term in &conjunction.terms {
+        let term_prob = storage.get_proposition_probability(term)?;
+        min_prob = min_prob.min(term_prob);
+    }
+
+    Ok(min_prob)
 }
