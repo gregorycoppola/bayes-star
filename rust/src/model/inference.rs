b6bd72ac6e727604935d22325c15d08449411952
Greg <greg@hiro.so>
2024-01-04 18:02:46 -0500

    add stub for inference.. need to handle the case where one of the needed probs is missing, and do recursive

--- rust/src/model/inference.rs
+++ rust/src/model/inference.rs
@@ -1,7 +1,36 @@
 use std::error::Error;
 
-use super::objects::Proposition;
+use crate::model::{maxent::{features_from_backlinks, compute_probability}, weights::read_weights, choose::compute_backlinks};
 
-pub fn inference_probability(proposition:&Proposition) -> Result<f64, Box<dyn Error>> {
-    Ok(0.5f64)
+use super::{objects::Proposition, storage::Storage};
+
+pub fn inference_probability(storage:&mut Storage, proposition:&Proposition) -> Result<f64, Box<dyn Error>> {
+    trace!("inference_probability - Start: {:?}", proposition);
+    trace!("inference_probability - Getting features from backlinks");
+    let backlinks = compute_backlinks(storage, &proposition)?;
+
+    let features = match features_from_backlinks(storage, &backlinks) {
+        Ok(f) => f,
+        Err(e) => {
+            trace!("inference_probability - Error in features_from_backlinks: {:?}", e);
+            return Err(e);
+        }
+    };
+
+    trace!("inference_probability - Reading weights");
+    let weight_vector = match read_weights(
+        storage.get_redis_connection(),
+        &features.keys().cloned().collect::<Vec<_>>(),
+    ) {
+        Ok(w) => w,
+        Err(e) => {
+            trace!("inference_probability - Error in read_weights: {:?}", e);
+            return Err(e);
+        }
+    };
+
+    trace!("inference_probability - Computing probability");
+    let probability = compute_probability(&weight_vector, &features);
+
+    Ok(probability)
 }
