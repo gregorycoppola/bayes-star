709fbb6721e444e1a8474a8f56b696f8937180d4
Greg <greg@hiro.so>
2024-01-04 11:47:43 -0500

    this is wrong [2024-01-04T16:47:03Z INFO  bayes_star::model::conjunction] Getting proposition probability for term 0: Proposition { roles: [FilledRole { role_name: "subject", argument: Variable(VariableArgument { domain: Jack }) }, FilledRole { role_name: "relation", argument: Constant(ConstantArgument { domain: Verb, entity_id: "like" }) }, FilledRole { role_name: "object", argument: Variable(VariableArgument { domain: Jill }) }] }

--- rust/src/model/conjunction.rs
+++ rust/src/model/conjunction.rs
@@ -6,11 +6,25 @@ pub fn get_conjunction_probability(
     storage: &mut Storage,
     conjunction: &Conjunction,
 ) -> Result<f64, Box<dyn Error>> {
+    info!("Calculating conjunction probability for a conjunction with {} terms", conjunction.terms.len());
     let mut min_prob = 1f64;
-    for term in &conjunction.terms {
-        let term_prob = storage.get_proposition_probability(term)?;
-        min_prob = min_prob.min(term_prob);
+
+    for (i, term) in conjunction.terms.iter().enumerate() {
+        info!("Getting proposition probability for term {}: {:?}", i, term);
+        
+        match storage.get_proposition_probability(term) {
+            Ok(term_prob) => {
+                info!("Term probability for term {}: {}", i, term_prob);
+                min_prob = min_prob.min(term_prob);
+                info!("Updated min probability after term {}: {}", i, min_prob);
+            },
+            Err(e) => {
+                error!("Error getting proposition probability for term {}: {}", i, e);
+                return Err(e);
+            },
+        }
     }
 
+    info!("Conjunction probability calculated: {}", min_prob);
     Ok(min_prob)
 }
