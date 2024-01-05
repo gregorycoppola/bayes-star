d9d67f443fe123ebb82755eef71b7a922774c83f
Greg <greg@hiro.so>
2024-01-04 19:26:21 -0500

    looks like it works but hard to see the reasoning... maybe need to show its work!
    [2024-01-05T00:25:40Z INFO  bayes_star::model::inference] Getting proposition probability for term 0: "[subject=test_Jill9,relation=exciting]"
    [2024-01-05T00:25:40Z INFO  bayes_star::model::inference] Getting proposition probability for term 0: "[subject=test_Jack9,relation=lonely]"
    [2024-01-05T00:25:40Z INFO  bayes_star::model::inference] inference_probability - Reading weights
    [2024-01-05T00:25:40Z INFO  bayes_star::model::inference] inference_probability - Computing probability
    [2024-01-05T00:25:40Z INFO  bayes_star::model::inference] inference_probability - Computed probability 0.9900864295373949 "[subject=test_Jack9,relation=like,object=test_Jill9]"
    [2024-01-05T00:25:40Z INFO  bayes_star::model::inference] Getting proposition probability for term 0: "[subject=test_Jill9,relation=like,object=test_Jack9]"
    [2024-01-05T00:25:40Z INFO  bayes_star::model::inference] inference_probability - Reading weights
    [2024-01-05T00:25:40Z INFO  bayes_star::model::inference] inference_probability - Computing probability
    [2024-01-05T00:25:40Z INFO  bayes_star::model::inference] inference_probability - Computed probability 0.9930219595391694 "[subject=test_Jack9,relation=date,object=test_Jill9]"

--- rust/train.sh
+++ rust/train.sh
@@ -1,1 +1,1 @@
-RUST_BACKTRACE=1 RUST_LOG=info cargo run -- --print_training_loss --entities_per_domain=8
+RUST_BACKTRACE=1 RUST_LOG=info cargo run -- --print_training_loss --entities_per_domain=1024
