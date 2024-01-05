4b408de1516aefc5a9e0c4b18e2fa949f60d6870
Greg <greg@hiro.so>
2024-01-05 18:21:31 -0500

    added code to compute joint probablity but seems werid
    INFO [src/model/inference.rs:255] direct probability 0.583534879762581 "[subject=test_Jack9,relation=date,object=test_Jill9]", {"[subject=test_Jill9,relation=like,object=test_Jack9]": true, "[subject=test_Jack9,relation=like,object=test_Jill9]": true}
    INFO [src/model/inference.rs:112] Starting computation of joint probability
    INFO [src/model/inference.rs:115] Processing event: [subject=test_Jill9,relation=like,object=test_Jack9]
    INFO [src/model/inference.rs:120] Event: [subject=test_Jill9,relation=like,object=test_Jack9], Probability: 1, Cumulative Probability: 1
    INFO [src/model/inference.rs:115] Processing event: [subject=test_Jack9,relation=like,object=test_Jill9]
    INFO [src/model/inference.rs:120] Event: [subject=test_Jack9,relation=like,object=test_Jill9], Probability: 0, Cumulative Probability: 0
    INFO [src/model/inference.rs:132] Final joint probability: 0
    INFO [src/model/inference.rs:263] joint parent probability 0 {"[subject=test_Jill9,relation=like,object=test_Jack9]": true, "[subject=test_Jack9,relation=like,object=test_Jill9]": true}
    WARN [src/main.rs:84] program done
    greg@Gregs-MacBook-Pro-2 rust % s
    On branch marginalize
    Your branch is up to date with 'origin/marginalize'.

--- rust/src/model/inference.rs
+++ rust/src/model/inference.rs
@@ -109,15 +109,18 @@ pub fn compute_joint_probability(
     assumed_probabilities: &HashMap<String, f64>,
 ) -> Result<f64, Box<dyn Error>> {
     let mut joint_probability = 1.0;
+    info!("\x1b[94mStarting computation of joint probability\x1b[0m");
 
     for (event, &is_true) in boolean_assignment {
+        info!("\x1b[94mProcessing event: {}\x1b[0m", event);
         match assumed_probabilities.get(event) {
             Some(&prob_true) => {
                 let prob = if is_true { prob_true } else { 1.0 - prob_true };
                 joint_probability *= prob;
-            }
+                info!("\x1b[94mEvent: {}, Probability: {}, Cumulative Probability: {}\x1b[0m", event, prob, joint_probability);
+            },
             None => {
-                // Handle the error case where the probability is not found
+                error!("\x1b[94mError: Probability not found for event: {}\x1b[0m", event);
                 return Err(Box::new(std::io::Error::new(
                     std::io::ErrorKind::NotFound,
                     format!("Probability not found for event: {}", event),
@@ -126,8 +129,10 @@ pub fn compute_joint_probability(
         }
     }
 
+    info!("\x1b[94mFinal joint probability: {}\x1b[0m", joint_probability);
     Ok(joint_probability)
 }
+
 
 pub fn local_inference_probability(
     storage: &mut Storage,
