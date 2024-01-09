a5430c2ab0cc568cb7e298902fb7763ed8a98f8e
Greg <greg@hiro.so>
2024-01-09 07:01:36 -0500

    added in the redis convenience operators

--- rust/src/common/redis.rs
+++ rust/src/common/redis.rs
@@ -21,3 +21,18 @@ pub fn set_members(conn: &mut Connection, key: &str) -> Result<Vec<String>, Box<
     let members: Vec<String> = conn.smembers(key)?;
     Ok(members)
 }
+
+pub fn seq_push(conn: &mut Connection, key: &str, value: &str) -> Result<i64, Box<dyn Error>> {
+    let length: i64 = conn.rpush(key, value)?;
+    Ok(length)
+}
+
+pub fn seq_pop(conn: &mut Connection, key: &str) -> Result<Option<String>, Box<dyn Error>> {
+    let value: Option<String> = conn.lpop(key, None)?;
+    Ok(value)
+}
+
+pub fn seq_get_all(conn: &mut Connection, key: &str) -> Result<Vec<String>, Box<dyn Error>> {
+    let elements: Vec<String> = conn.lrange(key, 0, -1)?;
+    Ok(elements)
+}
