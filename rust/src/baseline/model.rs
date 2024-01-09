4192d2ca159d9fc1ae4b6fdf9bb759a32d94e48b
Greg <greg@hiro.so>
2024-01-09 18:51:32 -0500

    says its unused

--- rust/src/baseline/model.rs
+++ rust/src/baseline/model.rs
@@ -1,9 +1,13 @@
+use std::{collections::HashMap, error::Error};
+use crate::model::objects::Conjunction;
+
 pub struct MonolithicBayes {
+    underlying:HashMap<Conjunction, f64>,
 }
 
 
 impl MonolithicBayes {
     pub fn new() -> Result<Self, Box<dyn Error>> {
-        Ok(MonolithicBayes{})
+        Ok(MonolithicBayes{ underlying: HashMap::new() })
     }
 }
