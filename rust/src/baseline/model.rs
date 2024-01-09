091a77cfbba62cbde3ebb1cf5986489fa050ee07
Greg <greg@hiro.so>
2024-01-09 18:45:29 -0500

    add stub baseline

---
+++ rust/src/baseline/model.rs
@@ -0,0 +1,9 @@
+pub struct MonolithicBayes {
+}
+
+
+impl MonolithicBayes {
+    pub fn new() -> Result<Self, Box<dyn Error>> {
+        Ok(MonolithicBayes{})
+    }
+}
--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,6 +1,7 @@
 pub mod model;
 pub mod scenarios;
 pub mod common;
+pub mod baseline;
 
 #[macro_use]
 extern crate log;
