c5c54e12a1e2a4565fbfe10c41c9ac21ca655fa9
Greg <greg@hiro.so>
2024-01-09 06:57:46 -0500

    added set ops

--- rust/src/common/redis.rs
+++ rust/src/common/redis.rs
@@ -2,13 +2,22 @@ use redis::Commands;
 use redis::Connection;
 use std::error::Error;
 
-pub fn insert_into_map(conn: &mut Connection, key: &str, field: &str, value: &str) -> Result<(), Box<dyn Error>> {
+pub fn map_insert(conn: &mut Connection, key: &str, field: &str, value: &str) -> Result<(), Box<dyn Error>> {
     conn.hset(key, field, value)?;
     Ok(())
 }
 
-
-pub fn read_from_map(conn: &mut Connection, key: &str, field: &str) -> Result<Option<String>, Box<dyn Error>> {
+pub fn map_get(conn: &mut Connection, key: &str, field: &str) -> Result<Option<String>, Box<dyn Error>> {
     let value: Option<String> = conn.hget(key, field)?;
     Ok(value)
+}
+
+pub fn set_add(conn: &mut Connection, key: &str, member: &str) -> Result<bool, Box<dyn Error>> {
+    let added: bool = conn.sadd(key, member)?;
+    Ok(added)
+}
+
+pub fn set_members(conn: &mut Connection, key: &str) -> Result<Vec<String>, Box<dyn Error>> {
+    let members: Vec<String> = conn.smembers(key)?;
+    Ok(members)
 }
