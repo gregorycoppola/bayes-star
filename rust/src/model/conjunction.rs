9a0bce6cd1bc2dc3d2aa81001ddb7f3e4e959e94
Greg <greg@hiro.so>
2024-01-10 02:27:42 -0500

    "fix"/"improve" the "conjunction probability" by using a product which it always should ahve been.. but really need to get th ebelief propagation working

--- rust/src/model/conjunction.rs
+++ rust/src/model/conjunction.rs
@@ -8,7 +8,7 @@ pub fn get_conjunction_probability<T: PropositionProbability>(
     conjunction: &Conjunction,
 ) -> Result<f64, Box<dyn Error>> {
     trace!("Calculating conjunction probability for a conjunction with {} terms", conjunction.terms.len());
-    let mut min_prob = 1f64;
+    let mut product = 1f64;
 
     for (i, term) in conjunction.terms.iter().enumerate() {
         trace!("Getting proposition probability for term {}: {:?}", i, term);
@@ -17,7 +17,7 @@ pub fn get_conjunction_probability<T: PropositionProbability>(
             Ok(term_prob_opt) => {
                 let term_prob = term_prob_opt.expect("`term_prob` should be Some here.");
                 trace!("Term probability for term {}: {}", term.search_string(), term_prob);
-                min_prob = min_prob.min(term_prob);
+                product *= term_prob;
                 trace!("Updated min probability after term {}: {}", term.search_string(), min_prob);
             },
             Err(e) => {
@@ -28,5 +28,5 @@ pub fn get_conjunction_probability<T: PropositionProbability>(
     }
 
     trace!("Conjunction probability calculated: {}", min_prob);
-    Ok(min_prob)
+    Ok(product)
 }
