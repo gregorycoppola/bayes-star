8dd9ff1632c2970312af2e3ca19bc92755e3d127
Greg <greg@hiro.so>
2024-01-10 01:12:16 -0500

    error[E0599]: no method named `get_redis_connection` found for mutable reference `&mut Storage` in the current scope    --> src/model/maxent.rs:144:21     | 144 |             storage.get_redis_connection(),     |                     ^^^^^^^^^^^^^^^^^^^^ method not found in `&mut Storage`
    error[E0599]: no method named `get_redis_connection` found for mutable reference `&mut Storage` in the current scope
       --> src/model/maxent.rs:179:30
        |
    179 |         save_weights(storage.get_redis_connection(), &new_weight)?;
        |                              ^^^^^^^^^^^^^^^^^^^^ method not found in `&mut Storage`
    
    error[E0599]: no method named `get_redis_connection` found for mutable reference `&mut Storage` in the current scope
       --> src/model/inference.rs:171:21
        |
    171 |             storage.get_redis_connection(),
        |                     ^^^^^^^^^^^^^^^^^^^^ method not found in `&mut Storage`

--- rust/src/model/weights.rs
+++ rust/src/model/weights.rs
@@ -1,5 +1,5 @@
 use redis::{Commands, Connection};
-use std::error::Error;
+use std::{error::Error, cell::RefCell};
 use crate::model::objects::Implication;
 use rand::Rng;
 use std::collections::HashMap;
@@ -19,81 +19,78 @@ pub fn negative_feature(feature: &str, class_label : usize) -> String {
     format!("->{} {}", class_label, feature)
 }
 
-// pub fn initialize_weights(con: &mut Connection, implication: &Implication) -> Result<(), Box<dyn Error>> {
-//     trace!("initialize_weights - Start: {:?}", implication);
+struct ExponentialWeights {
+    connection:RefCell<Connection>,
+}
 
-//     let feature = implication.unique_key();
-//     trace!("initialize_weights - Unique key: {}", feature);
-
-//     for class_label in CLASS_LABELS {
-//         let posf = positive_feature(&feature, class_label);
-//         let negf = negative_feature(&feature, class_label);
-//         trace!("initialize_weights - Positive feature: {}, Negative feature: {}", posf, negf);
+impl ExponentialWeights {
+    pub fn initialize_weights(&mut self, implication: &Implication) -> Result<(), Box<dyn Error>> {
+        trace!("initialize_weights - Start: {:?}", implication);
+        let feature = implication.unique_key();
+        trace!("initialize_weights - Unique key: {}", feature);
+        for class_label in CLASS_LABELS {
+            let posf = positive_feature(&feature, class_label);
+            let negf = negative_feature(&feature, class_label);
+            trace!("initialize_weights - Positive feature: {}, Negative feature: {}", posf, negf);
+            let weight1 = random_weight();
+            let weight2 = random_weight();
+            trace!("initialize_weights - Generated weights: {}, {}", weight1, weight2);
+            trace!("initialize_weights - Setting positive feature weight");
+            self.connection.borrow_mut().hset("weights", &posf, weight1)
+                .map_err(|e| {
+                    trace!("initialize_weights - Error setting positive feature weight: {:?}", e);
+                    Box::new(e) as Box<dyn Error>
+                })?;
+            trace!("initialize_weights - Setting negative feature weight");
+            self.connection.borrow_mut().hset("weights", &negf, weight2)
+                .map_err(|e| {
+                    trace!("initialize_weights - Error setting negative feature weight: {:?}", e);
+                    Box::new(e) as Box<dyn Error>
+                })?;
+        }
+        trace!("initialize_weights - End");
+        Ok(())
+    }
     
-//         let weight1 = random_weight();
-//         let weight2 = random_weight();
-//         trace!("initialize_weights - Generated weights: {}, {}", weight1, weight2);
     
-//         trace!("initialize_weights - Setting positive feature weight");
-//         con.hset("weights", &posf, weight1)
-//             .map_err(|e| {
-//                 trace!("initialize_weights - Error setting positive feature weight: {:?}", e);
-//                 Box::new(e) as Box<dyn Error>
-//             })?;
-    
-//         trace!("initialize_weights - Setting negative feature weight");
-//         con.hset("weights", &negf, weight2)
-//             .map_err(|e| {
-//                 trace!("initialize_weights - Error setting negative feature weight: {:?}", e);
-//                 Box::new(e) as Box<dyn Error>
-//             })?;
-//     }
-
-
-//     trace!("initialize_weights - End");
-//     Ok(())
-// }
-
-
-pub fn read_weights(con: &mut Connection, features: &[String]) -> Result<HashMap<String, f64>, Box<dyn Error>> {
-    trace!("read_weights - Start");
-    let mut weights = HashMap::new();
-
-    for feature in features {
-        trace!("read_weights - Reading weight for feature: {}", feature);
-        match con.hget::<_, _, String>("weights", feature) {
-            Ok(record) => {
-                trace!("read_weights - Retrieved record: {}", record);
-                let weight = record.parse::<f64>()
-                    .map_err(|e| {
-                        trace!("read_weights - Error parsing weight: {:?}", e);
-                        Box::new(e) as Box<dyn Error>
-                    })?;
-                weights.insert(feature.clone(), weight);
-            }
-            Err(e) => {
-                trace!("read_weights - Error retrieving weight for feature {}: {:?}", feature, e);
-                return Err(Box::new(e) as Box<dyn Error>);
+    pub fn read_weights(&self, features: &[String]) -> Result<HashMap<String, f64>, Box<dyn Error>> {
+        trace!("read_weights - Start");
+        let mut weights = HashMap::new();
+        for feature in features {
+            trace!("read_weights - Reading weight for feature: {}", feature);
+            match self.connection.borrow_mut().hget::<_, _, String>("weights", feature) {
+                Ok(record) => {
+                    trace!("read_weights - Retrieved record: {}", record);
+                    let weight = record.parse::<f64>()
+                        .map_err(|e| {
+                            trace!("read_weights - Error parsing weight: {:?}", e);
+                            Box::new(e) as Box<dyn Error>
+                        })?;
+                    weights.insert(feature.clone(), weight);
+                }
+                Err(e) => {
+                    trace!("read_weights - Error retrieving weight for feature {}: {:?}", feature, e);
+                    return Err(Box::new(e) as Box<dyn Error>);
+                }
             }
         }
+        trace!("read_weights - End");
+        Ok(weights)
     }
-
-    trace!("read_weights - End");
-    Ok(weights)
+    
+    pub fn save_weights(&mut self, weights: &HashMap<String, f64>) -> Result<(), Box<dyn Error>> {
+        trace!("save_weights - Start");
+        for (feature, &value) in weights {
+            trace!("save_weights - Saving weight for feature {}: {}", feature, value);
+            self.connection.borrow_mut().hset("weights", feature, value)
+                .map_err(|e| {
+                    trace!("save_weights - Error saving weight for feature {}: {:?}", feature, e);
+                    Box::new(e) as Box<dyn Error>
+                })?;
+        }
+        trace!("save_weights - End");
+        Ok(())
+    }
 }
 
 
-pub fn save_weights(con: &mut Connection, weights: &HashMap<String, f64>) -> Result<(), Box<dyn Error>> {
-    trace!("save_weights - Start");
-    for (feature, &value) in weights {
-        trace!("save_weights - Saving weight for feature {}: {}", feature, value);
-        con.hset("weights", feature, value)
-            .map_err(|e| {
-                trace!("save_weights - Error saving weight for feature {}: {:?}", feature, e);
-                Box::new(e) as Box<dyn Error>
-            })?;
-    }
-
-    trace!("save_weights - End");
-    Ok(())
-}
