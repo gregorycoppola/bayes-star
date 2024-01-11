70ccb5d73f857f2dd6f16cf6c9a0447600b64392
Greg <greg@hiro.so>
2024-01-10 19:47:50 -0500

    error[E0407]: method `get_conjunction_probability` is not a member of trait `FactDB`    --> src/model/inference.rs:106:5     | 106 |       fn get_conjunction_probability(     |       ^  --------------------------- help: there is an associated function with a similar name: `get_proposition_probability`     |  _____|     | | 107 | |         &self, 108 | |         conjunction: &Conjunction, 109 | |     ) -> Result<Option<f64>, Box<dyn Error>> { 110 | |         todo!("tbd") 111 | |     }     | |_____^ not a member of trait `FactDB`
    error[E0407]: method `get_conjunction_probability` is not a member of trait `FactDB`
       --> src/common/model.rs:399:5
        |
    399 |       fn get_conjunction_probability(
        |       ^  --------------------------- help: there is an associated function with a similar name: `get_proposition_probability`
        |  _____|
        | |
    400 | |         &self,
    401 | |         conjunction: &Conjunction,
    402 | |     ) -> Result<Option<f64>, Box<dyn Error>> {
    ...   |
    409 | |         Ok(Some(result))
    410 | |     }
        | |_____^ not a member of trait `FactDB`
    
    error[E0425]: cannot find value `storage` in this scope
      --> src/model/maxent.rs:56:47
       |

--- rust/src/model/conjunction.rs
+++ rust/src/model/conjunction.rs
@@ -3,17 +3,17 @@ use crate::common::interface::FactDB;
 
 use super::objects::Conjunction;
 
-pub fn get_conjunction_probability<T: FactDB>(
-    storage: &T,
+/// Compute the "conjunction probability", but assume INDEPENDENCE.
+/// Therefore, the probability of the conjunct is the product of all of the probabilities of the terms.
+pub fn get_conjunction_probability(
+    fact_db: &dyn FactDB,
     conjunction: &Conjunction,
 ) -> Result<f64, Box<dyn Error>> {
     trace!("Calculating conjunction probability for a conjunction with {} terms", conjunction.terms.len());
     let mut product = 1f64;
-
     for (i, term) in conjunction.terms.iter().enumerate() {
         trace!("Getting proposition probability for term {}: {:?}", i, term);
-        
-        match storage.get_proposition_probability(term) {
+        match fact_db.get_proposition_probability(term) {
             Ok(term_prob_opt) => {
                 let term_prob = term_prob_opt.expect("`term_prob` should be Some here.");
                 trace!("Term probability for term {}: {}", term.search_string(), term_prob);
@@ -26,7 +26,6 @@ pub fn get_conjunction_probability<T: FactDB>(
             },
         }
     }
-
     trace!("Conjunction probability calculated: {}", product);
     Ok(product)
 }
