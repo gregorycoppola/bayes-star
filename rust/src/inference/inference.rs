d17bfa5aea4fc4cc1520723cf940b8870c059307
Greg <greg@hiro.so>
2024-01-11 09:40:38 -0500

    rename again

---
+++ rust/src/inference/inference.rs
@@ -0,0 +1,111 @@
+
+use std::{collections::HashMap, error::Error};
+use redis::Connection;
+use crate::{model::{objects::Proposition, weights::CLASS_LABELS}, common::interface::FactDB};
+use super::table::BeliefPropagationData;
+
+pub struct Inferencer {
+    data: BeliefPropagationData,
+    evidence: Box<dyn FactDB>,
+}
+
+impl Inferencer {
+    // Initialize new Storage with a Redis connection
+    pub fn new(evidence: Box<dyn FactDB>) -> Result<Self, redis::RedisError> {
+        Ok(Inferencer {
+            data: BeliefPropagationData::new(), evidence,
+        })
+    }
+
+    pub fn initialize(&mut self) -> Result<(), Box<dyn Error>> {
+        self.initialize_pi()?;
+        self.initialize_lambda()?;
+        Ok(())
+    }
+
+    pub fn initialize_pi(&mut self) -> Result<(), Box<dyn Error>> {
+        let root = self.find_root()?;
+        self.initialize_pi_node(&root, true)?;
+        Ok(())
+    }
+
+    pub fn initialize_pi_node(&mut self, node:&Proposition, is_root:bool) -> Result<(), Box<dyn Error>> {
+        let children = self.find_children(node)?;
+        for child in &children {
+            self.initialize_pi_node(child, false)?;
+        }
+
+        if is_root {
+            let prior_prob = self.get_proposition_probability(node)?;
+            self.data.set_pi_value(node, 1, prior_prob);
+            self.data.set_pi_value(node, 0, 1f64 - prior_prob);
+        }
+        for outcome in CLASS_LABELS {
+            let children =  self.find_children(node).expect("Error finding children");
+            for child in &children {
+                self.data.set_lambda_message(node, child, outcome, 1f64);
+            }
+        }
+    
+        Ok(())
+    }
+
+
+    pub fn initialize_lambda(&mut self) -> Result<(), Box<dyn Error>> {
+        let root = self.find_root()?;
+        self.initialize_lambda_node(&root)?;
+        Ok(())
+    }
+
+    pub fn initialize_lambda_node(&mut self, node:&Proposition) -> Result<(), Box<dyn Error>> {
+        for outcome in CLASS_LABELS {
+            self.data.set_lambda_value(node, outcome, 1f64);
+            let parents =  &self.find_parent(node).expect("Error finding paerents").expect("No parents");
+            for parent in &[parents] {
+                self.data.set_lambda_message(node, parent, outcome, 1f64);
+            }
+        }
+        let children = self.find_children(node)?;
+        for child in &children {
+            self.initialize_lambda_node(child)?;
+        }
+        Ok(())
+    }
+
+    pub fn get_all_propositions(&self) -> Result<Vec<Proposition>, Box<dyn Error>> {
+        // Your implementation here
+        todo!()
+    }
+
+    pub fn get_proposition_probability(
+        &self,
+        proposition: &Proposition,
+    ) -> Result<f64, Box<dyn Error>> {
+        // Your implementation here
+        todo!()
+    }
+
+    pub fn get_conditional_probability(
+        &self,
+        conclusion: &Proposition,
+        premise: &Proposition,
+    ) -> Result<f64, Box<dyn Error>> {
+        // Your implementation here
+        todo!()
+    }
+
+    fn find_parent(&self, x: &Proposition) -> Result<Option<Proposition>, Box<dyn Error>> {
+        // Your implementation here
+        Ok(None) // Placeholder
+    }
+
+    fn find_root(&self) -> Result<Proposition, Box<dyn Error>> {
+        // Your implementation here
+        todo!()
+    }
+
+    fn find_children(&self, root: &Proposition) -> Result<Vec<Proposition>, Box<dyn Error>> {
+        // Your implementation here
+        Ok(Vec::new()) // Placeholder
+    }
+}
