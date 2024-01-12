770cd8869c8933df902b0a783338661f166ab8c4
Greg <greg@hiro.so>
2024-01-12 18:55:07 -0500

    add stubs for both

--- rust/src/inference/implications.rs
+++ rust/src/inference/implications.rs
@@ -1,6 +1,9 @@
 use std::{error::Error, rc::Rc};
 
-use crate::{common::{graph::Graph, redis::RedisManager}, model::objects::{Proposition, PropositionConjunction}};
+use crate::{
+    common::{graph::Graph, redis::RedisManager},
+    model::objects::{Proposition, PropositionConjunction},
+};
 
 pub struct PropositionGraph {
     predicate_graph: Rc<Graph>,
@@ -8,7 +11,7 @@ pub struct PropositionGraph {
 
 impl PropositionGraph {
     fn new(predicate_graph: Rc<Graph>) -> Result<PropositionGraph, Box<dyn Error>> {
-        Ok(PropositionGraph{ predicate_graph })
+        Ok(PropositionGraph { predicate_graph })
     }
     pub fn find_roots(
         &self,
@@ -42,6 +45,16 @@ impl PropositionGraph {
     }
 }
 
-pub fn compute_forward_graph(predicate_graph:Rc<Graph>) -> Result<PropositionGraph, Box<dyn Error>> {
+pub fn compute_forward_graph(
+    predicate_graph: Rc<Graph>,
+    proposition: &Proposition,
+) -> Result<PropositionGraph, Box<dyn Error>> {
+    todo!()
+}
+
+pub fn compute_backward_graph(
+    predicate_graph: Rc<Graph>,
+    proposition: &Proposition,
+) -> Result<PropositionGraph, Box<dyn Error>> {
     todo!()
 }
