2cd7cfc2b03539ad06b3ad59c3f367913e5160c8
Greg <greg@hiro.so>
2024-01-12 22:07:58 -0500

    better runttest

--- rust/runtest.sh
+++ rust/runtest.sh
@@ -1,1 +1,11 @@
-cargo test -- --test-threads=1 --nocapture
+#!/bin/bash
+
+# Check if an argument (test name) is provided
+if [ "$#" -eq 1 ]; then
+    # Run only the specified test
+    cargo test $1 -- --test-threads=1 --nocapture
+else
+    # Run all tests
+    cargo test -- --test-threads=1 --nocapture
+fi
+
