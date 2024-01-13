a8213a71e3e37601923b3ecccab21e22a61b9842
Greg <greg@hiro.so>
2024-01-13 18:09:03 -0500

    maybe just change these
    error[E0599]: no method named `find_roots` found for struct `Box<PropositionGraph>` in the current scope
      --> src/inference/inference.rs:55:44
       |
    55 |         let roots = self.proposition_graph.find_roots(proposition)?;
       |                                            ^^^^^^^^^^ method not found in `Box<PropositionGraph>`
    
    error[E0599]: no method named `proposition_forward_links` found for struct `Box<PropositionGraph>` in the current scope
      --> src/inference/inference.rs:67:47
       |
    67 |         let children = self.proposition_graph.proposition_forward_links(node)?;
       |                                               ^^^^^^^^^^^^^^^^^^^^^^^^^ method not found in `Box<PropositionGraph>`
    
    error[E0599]: no method named `proposition_forward_links` found for struct `Box<PropositionGraph>` in the current scope
      --> src/inference/inference.rs:84:18
       |
    84 |                 .proposition_forward_links(node)
       |                  ^^^^^^^^^^^^^^^^^^^^^^^^^ method not found in `Box<PropositionGraph>`

--- rust/src/inference/graph.rs
+++ rust/src/inference/graph.rs
@@ -80,55 +80,6 @@ impl PropositionGraph {
         initialize_visit_single(&mut graph, target)?;
         Ok(Rc::new(graph))
     }
-    pub fn find_roots(
-        &self,
-        proposition: &Proposition,
-    ) -> Result<Vec<Proposition>, Box<dyn Error>> {
-        todo!()
-    }
-    pub fn proposition_backward_links(
-        &self,
-        proposition: &Proposition,
-    ) -> Result<Vec<PropositionGroup>, Box<dyn Error>> {
-        let search_predicates = compute_search_predicates(proposition)?;
-        let mut result = vec![];
-        for predicate in &search_predicates {
-            let predicate_backward = self.predicate_graph.predicate_backward_links(predicate)?;
-            for inference_link in &predicate_backward {
-                let proposition_implication =
-                    proposition_implication_from(inference_link, proposition)?;
-                result.push(proposition_implication.premise);
-            }
-        }
-        Ok(result)
-    }
-    pub fn proposition_forward_links(
-        &self,
-        proposition: &Proposition,
-    ) -> Result<Vec<PropositionGroup>, Box<dyn Error>> {
-        todo!()
-        // let search_predicates = compute_search_predicates(proposition)?;
-        // let mut result = vec![];
-        // for predicate in &search_predicates {
-        //     let predicate_backward = self.predicate_graph.predicate_forward_links(predicate)?;
-        //     for inference_link in &predicate_backward {
-        //         result.push(inference_link);
-        //     }
-        // }
-        // Ok(result)
-    }
-    pub fn conjoined_backward_links(
-        &self,
-        conjoined: &PropositionGroup,
-    ) -> Result<Vec<Proposition>, Box<dyn Error>> {
-        Ok(conjoined.terms.clone())
-    }
-    pub fn conjoined_forward_links(
-        &self,
-        conjoined: &PropositionGroup,
-    ) -> Result<Vec<Proposition>, Box<dyn Error>> {
-        todo!()
-    }
 }
 
 pub fn compute_forward_graph(
