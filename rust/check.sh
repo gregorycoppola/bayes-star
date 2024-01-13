5b302cf710433eb63bf0b516339b2f821a76a5a9
Greg <greg@hiro.so>
2024-01-13 17:03:03 -0500

    don't check tests if first check fails

--- rust/check.sh
+++ rust/check.sh
@@ -1,2 +1,15 @@
+#!/bin/bash
+
+# Run cargo check
 cargo check
-cargo check --tests
+
+# Check the exit status of the last command (cargo check)
+if [ $? -eq 0 ]; then
+    # If cargo check was successful, run cargo check for tests
+    cargo check --tests
+else
+    # If cargo check failed, exit the script
+    echo "cargo check failed, aborting script."
+    exit 1
+fi
+
