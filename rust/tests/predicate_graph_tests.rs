2d54c0f84ca615d8c990547a5cb02c9e50fc0e78
Greg <greg@hiro.so>
2024-01-12 20:20:01 -0500

    running 1 test test test_store_entity ... FAILED
    failures:
    
    ---- test_store_entity stdout ----
    thread 'test_store_entity' panicked at 'not yet implemented', src/common/resources.rs:14:9
    note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace

--- rust/tests/predicate_graph_tests.rs
+++ rust/tests/predicate_graph_tests.rs
@@ -1,12 +1,18 @@
-use bayes_star::{scenarios::dating_simple::SimpleDating, common::{interface::ScenarioMaker, redis::RedisManager}};
+use bayes_star::{
+    common::{interface::ScenarioMaker, redis::RedisManager, resources::FactoryResources},
+    scenarios::dating_simple::SimpleDating,
+};
 use log::info;
-
+use bayes_star::model::config::ConfigurationOptions;
 
 #[test]
 fn test_store_entity() {
-    let redis_client = RedisManager::new().unwrap();
-    let scenario_maker = SimpleDating{};
-    let result = scenario_maker.setup_scenario(
-        &redis_client);
+    let config = ConfigurationOptions {
+        entities_per_domain: 12,
+        print_training_loss: false,
+    };
+    let resources = FactoryResources::new(&config).expect("Couldn't create resources.");
+    let scenario_maker = SimpleDating {};
+    let result = scenario_maker.setup_scenario(&resources);
     info!("scenario result: {:?}", result);
 }
