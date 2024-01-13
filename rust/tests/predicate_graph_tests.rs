0f2ae562c27ea419696cba78f55a9cc30a0e2338
Greg <greg@hiro.so>
2024-01-12 20:46:30 -0500

    get all predicates test kind of working

--- rust/tests/predicate_graph_tests.rs
+++ rust/tests/predicate_graph_tests.rs
@@ -1,6 +1,8 @@
+use std::collections::HashMap;
+
 use bayes_star::{
     common::{interface::ScenarioMaker, redis::RedisManager, resources::FactoryResources, graph::PredicateGraph},
-    scenarios::dating_simple::SimpleDating, inference::implications,
+    scenarios::dating_simple::SimpleDating, inference::implications, model::{creators::{predicate, implication, conjunction, subject, relation, object, constant, variable}, objects::{RoleMap, Domain}},
 };
 use log::info;
 use bayes_star::model::config::ConfigurationOptions;
@@ -16,9 +18,80 @@ fn test_store_entity() {
     let scenario_maker = SimpleDating {};
     let result = scenario_maker.setup_scenario(&resources);
     println!("scenario result: {:?}", result);
-    let graph = PredicateGraph::new(&resources.redis).unwrap();
-    let implications = graph.get_all_implications().unwrap();
+    let predicate_graph = PredicateGraph::new(&resources.redis).unwrap();
+    let implications = predicate_graph.get_all_implications().unwrap();
     for implication in &implications {
         println!("implication {:?}", implication);
     }
+
+    let exciting = constant(Domain::Verb, "exciting".to_string());
+    let lonely = constant(Domain::Verb, "lonely".to_string());
+    let like = constant(Domain::Verb, "like".to_string());
+    let date = constant(Domain::Verb, "date".to_string());
+    let xjack = variable(Domain::Jack);
+    let xjill = variable(Domain::Jill);
+    let implications = vec![
+            // if jack is lonely, he will date any jill
+            implication(
+                conjunction(vec![predicate(vec![
+                    subject(xjack.clone()),
+                    relation(lonely.clone()),
+                ])]),
+                predicate(vec![
+                    subject(xjack.clone()),
+                    relation(like.clone()),
+                    object(xjill.clone()),
+                ]),
+                vec![RoleMap::new(HashMap::from([(
+                    "subject".to_string(),
+                    "subject".to_string(),
+                )]))],
+            ),
+            // if jill is exciting, any jack will date her
+            implication(
+                conjunction(vec![predicate(vec![
+                    subject(xjill.clone()),
+                    relation(exciting.clone()),
+                ])]),
+                predicate(vec![
+                    subject(xjack.clone()),
+                    relation(like.clone()),
+                    object(xjill.clone()),
+                ]),
+                vec![RoleMap::new(HashMap::from([(
+                    "object".to_string(),
+                    "subject".to_string(),
+                )]))],
+            ),
+            // if jill likes jack, then jack dates jill
+            implication(
+                conjunction(vec![
+                    predicate(vec![
+                        subject(xjill.clone()),
+                        relation(like.clone()),
+                        object(xjack.clone()),
+                    ]),
+                    predicate(vec![
+                        subject(xjack.clone()),
+                        relation(like.clone()),
+                        object(xjill.clone()),
+                    ]),
+                ]),
+                predicate(vec![
+                    subject(xjack.clone()),
+                    relation(date.clone()),
+                    object(xjill.clone()),
+                ]),
+                vec![
+                    RoleMap::new(HashMap::from([
+                        ("subject".to_string(), "object".to_string()),
+                        ("object".to_string(), "subject".to_string()),
+                    ])),
+                    RoleMap::new(HashMap::from([
+                        ("subject".to_string(), "subject".to_string()),
+                        ("object".to_string(), "object".to_string()),
+                    ])),
+                ],
+            ),
+        ];
 }
