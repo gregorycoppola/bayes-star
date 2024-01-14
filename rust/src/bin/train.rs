016a8a39e886b3331c5c072477917bb31550f620
Greg <greg@hiro.so>
2024-01-14 10:54:45 -0500

    format

--- rust/src/bin/train.rs
+++ rust/src/bin/train.rs
@@ -1,7 +1,7 @@
 use std::borrow::Borrow;
 
 use bayes_star::common::setup::parse_configuration_options;
-use bayes_star::common::{run::setup_and_train, resources::FactoryResources};
+use bayes_star::common::{resources::FactoryResources, run::setup_and_train};
 use bayes_star::scenarios::factory::ScenarioMakerFactory;
 
 #[macro_use]
@@ -10,7 +10,8 @@ extern crate log;
 fn main() {
     let config = parse_configuration_options();
     let resources = FactoryResources::new(&config).expect("Couldn't create resources.");
-    let scenario_maker = ScenarioMakerFactory::new_shared(&"DatingSimple".to_string()).expect("Could not make scenario maker.");
+    let scenario_maker = ScenarioMakerFactory::new_shared(&"DatingSimple".to_string())
+        .expect("Could not make scenario maker.");
     setup_and_train(&resources, scenario_maker.borrow()).expect("Error in training.");
     warn!("program done");
 }
