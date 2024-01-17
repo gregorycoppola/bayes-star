fa67307957d1f2d0d4d7835f7e903a616f2217cb
Greg <greg@hiro.so>
2024-01-17 10:01:46 -0500

    not training this factor :(
    INFO [src/common/train.rs:107] GraphicalModel::get_propositions_from_queue - Start. Queue name: training_queue
    INFO [src/common/redis.rs:75] seq_get_all training_queue
    INFO [src/common/train.rs:117] GraphicalModel::get_propositions_from_queue - Retrieved and deserialized propositions successfully
    INFO [src/common/train.rs:152] do_training - Processing propositions: 922
    INFO [src/common/train.rs:158] do_training - Processing proposition: Constant(ConstantArgument { domain: Verb, entity_id: "exciting" })[sub=train_Jack0]
    INFO [src/common/train.rs:160] do_training - Backimplications: []
    INFO [src/common/train.rs:158] do_training - Processing proposition: Constant(ConstantArgument { domain: Verb, entity_id: "exciting" })[sub=train_Jack1]

--- rust/src/scenarios/mod.rs
+++ rust/src/scenarios/mod.rs
@@ -1,3 +1,4 @@
 pub mod factory;
 // scenarios
 pub mod dating_simple;
+pub mod one_var;
---
+++ rust/src/scenarios/one_var.rs
@@ -0,0 +1,109 @@
+use crate::common::proposition_db::RedisFactDB;
+use crate::common::graph::InferenceGraph;
+use crate::common::interface::PropositionDB;
+use crate::common::model::InferenceModel;
+use crate::common::redis::RedisManager;
+use crate::common::resources::{self, FactoryResources};
+use crate::common::train::TrainingPlan;
+use crate::model::creators::predicate;
+use crate::{
+    common::interface::ScenarioMaker,
+    model::{
+        creators::{conjunction, constant, implication, obj, proposition, sub, variable},
+        objects::{Domain, Entity, RoleMap},
+    },
+};
+use rand::Rng; // Import Rng trait
+use std::{collections::HashMap, error::Error};
+fn cointoss() -> f64 {
+    let mut rng = rand::thread_rng(); // Get a random number generator
+    if rng.gen::<f64>() < 0.5 {
+        1.0
+    } else {
+        0.0
+    }
+}
+
+fn weighted_cointoss(threshold: f64) -> f64 {
+    let mut rng = rand::thread_rng(); // Get a random number generator
+    if rng.gen::<f64>() < threshold {
+        1.0
+    } else {
+        0.0
+    }
+}
+
+pub struct OneVariable {}
+
+impl ScenarioMaker for OneVariable {
+    fn setup_scenario(
+        &self,
+        resources: &FactoryResources,
+    ) -> Result<(), Box<dyn Error>> {
+        let mut graph = InferenceGraph::new_mutable(resources)?;
+        let mut proposition_db = RedisFactDB::new_mutable(&resources.redis)?;
+        let mut plan = TrainingPlan::new(&resources.redis)?;
+        let config = &resources.config;
+        let total_members_each_class = config.entities_per_domain;
+
+        // Retrieve entities in the Jack domain
+        let jack_domain = Domain::Jack.to_string(); // Convert enum to string and make lowercase
+        let jacks: Vec<Entity> = graph.get_entities_in_domain(&jack_domain)?;
+        trace!("Initial number of jacks: {}", jacks.len());
+        let exciting = constant(Domain::Verb, "exciting".to_string());
+
+        for i in 0..total_members_each_class {
+            let is_test = i % 10 == 9;
+            let is_training = !is_test;
+            let mut domain_entity_map: HashMap<String, Entity> = HashMap::new();
+
+            for domain in [Domain::Jack].iter() {
+                let prefix = if is_test { "test" } else { "train" };
+                let name = format!("{}_{:?}{}", &prefix, domain, i); // Using Debug formatting for Domain enum
+                let entity = Entity {
+                    domain: domain.clone(),
+                    name: name.clone(),
+                };
+                graph.store_entity(&entity)?;
+                trace!("Stored entity: {:?}", &entity);
+                domain_entity_map.insert(domain.to_string(), entity);
+            }
+
+            let jack_entity = &domain_entity_map[&Domain::Jack.to_string()];
+            let p_jack_exciting = weighted_cointoss(0.3f64);
+            {
+                trace!("Jack entity part 2: {:?}", jack_entity);
+                let jack = constant(jack_entity.domain, jack_entity.name.clone());
+                let jack_lonely = proposition(exciting.to_string(), vec![sub(jack)]);
+
+                trace!(
+                    "Jack Lonely: {:?}, Probability: {}",
+                    jack_lonely.predicate.hash_string(),
+                    p_jack_exciting
+                );
+                proposition_db.store_proposition_probability(&jack_lonely, p_jack_exciting)?;
+                plan.maybe_add_to_training(is_training, &jack_lonely)?;
+            }
+        }
+
+        let xjack = variable(Domain::Jack);
+        let xjill = variable(Domain::Jill);
+        let implications = vec![];
+        for implication in implications.iter() {
+            trace!("Storing implication: {:?}", implication); // Logging, replace with your actual logger if necessary
+            // Assuming `store_implication` is a method of your GraphicalModel struct
+            graph.store_predicate_implication(implication)?;
+        }
+
+        // Additional functions
+        fn numeric_or(a: f64, b: f64) -> f64 {
+            f64::min(a + b, 1.0)
+        }
+
+        fn numeric_and(a: f64, b: f64) -> f64 {
+            a * b
+        }
+
+        Ok(())
+    }
+}
