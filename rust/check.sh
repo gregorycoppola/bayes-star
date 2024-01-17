2f1b2e61ed419930f8080bc871323a8248c650a6
Greg <greg@hiro.so>
2024-01-16 22:33:19 -0500

    change check.sh to avoid checking tests for now

--- rust/check.sh
+++ rust/check.sh
@@ -5,8 +5,13 @@ cargo check
 
 # Check the exit status of the last command (cargo check)
 if [ $? -eq 0 ]; then
-    # If cargo check was successful, run cargo check for tests
-    cargo check --tests
+    # Check if the first argument is "test"
+    if [ "$1" == "test" ]; then
+        # If cargo check was successful and first argument is "test", run cargo check for tests
+        cargo check --tests
+    else
+        echo "Skipping test checks, as the first argument is not 'test'."
+    fi
 else
     # If cargo check failed, exit the script
     echo "cargo check failed, aborting script."
