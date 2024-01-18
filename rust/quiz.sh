a54ff33945564acf9fbd4d4784078c9a2b12ea3e
Greg <greg@hiro.so>
2024-01-18 00:51:04 -0500

    better quiz

--- rust/quiz.sh
+++ rust/quiz.sh
@@ -1,1 +1,2 @@
-RUST_BACKTRACE=1 RUST_LOG=info cargo run --bin test -- --print_training_loss --entities_per_domain=1024
+RUST_BACKTRACE=1 RUST_LOG=info cargo run --bin test -- --print_training_loss --entities_per_domain=1024 2> output/log
+cat output/log
