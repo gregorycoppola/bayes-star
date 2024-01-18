6340ff8e6d213f1f9605f50a1a86a6a04be8d0b0
Greg <greg@hiro.so>
2024-01-18 17:10:44 -0500

    make pi a separate file

--- rust/src/inference/pi.rs
+++ rust/src/inference/pi.rs
@@ -1,6 +1,6 @@
 use std::error::Error;
-use crate::{print_red, print_yellow, model::{objects::EXISTENCE_FUNCTION, weights::CLASS_LABELS}};
-use super::{inference::Inferencer, table::PropositionNode};
+use crate::{print_red, print_yellow, model::{objects::EXISTENCE_FUNCTION, weights::CLASS_LABELS}, print_green, print_blue};
+use super::{inference::{Inferencer, groups_from_backlinks, compute_each_combination}, table::{PropositionNode, GenericNodeType}};
 
 impl Inferencer {
     pub fn send_pi_messages(&mut self) -> Result<(), Box<dyn Error>> {
@@ -71,6 +71,108 @@ impl Inferencer {
             }
         }
         // Success.
+        Ok(())
+    }
+
+    pub fn pi_compute_generic(&mut self, node: &PropositionNode) -> Result<(), Box<dyn Error>> {
+        match &node.node {
+            GenericNodeType::Single(proposition) => {
+                self.pi_compute_single(node)?;
+            }
+            GenericNodeType::Group(group) => {
+                self.pi_compute_group(node)?;
+            }
+        }
+        Ok(())
+    }
+
+    // from_node is a single.. compute it from the group
+    pub fn pi_compute_single(&mut self, node: &PropositionNode) -> Result<(), Box<dyn Error>> {
+        let conclusion = node.extract_single();
+        let parent_nodes = self.proposition_graph.get_all_backward(node);
+        let premise_groups = groups_from_backlinks(&parent_nodes);
+        let all_combinations = compute_each_combination(&parent_nodes);
+        let mut sum_true = 0f64;
+        let mut sum_false = 0f64;
+        for combination in &all_combinations {
+            let mut product = 1f64;
+            for (index, parent_node) in parent_nodes.iter().enumerate() {
+                let boolean_outcome = combination.get(parent_node).unwrap();
+                let usize_outcome = if *boolean_outcome { 1 } else { 0 };
+                let pi_x_z = self
+                    .data
+                    .get_pi_message(parent_node, node, usize_outcome)
+                    .unwrap();
+                print_red!(
+                    "getting pi message parent_node {:?}, node {:?}, usize_outcome {}, pi_x_z {}",
+                    &parent_node,
+                    &node,
+                    usize_outcome,
+                    pi_x_z,
+                );
+                product *= pi_x_z;
+            }
+            let factor =
+                self.build_factor_context_for_assignment(&premise_groups, combination, &conclusion);
+            let prediction = self.model.model.predict(&factor)?;
+            print_yellow!("local probability {}  for factor {:?}", &prediction.marginal, &factor);
+            let true_marginal = &prediction.marginal;
+            let false_marginal = 1f64 - true_marginal;
+            sum_true += true_marginal * product;
+            sum_false += false_marginal * product;
+        }
+        self.data.set_pi_value(node, 1, sum_true);
+        self.data.set_pi_value(node, 0, sum_false);
+        Ok(())
+    }
+
+    pub fn pi_compute_group(&mut self, node: &PropositionNode) -> Result<(), Box<dyn Error>> {
+        let parent_nodes = self.proposition_graph.get_all_backward(node);
+        print_yellow!("pi_compute_group {:?}", &parent_nodes);
+        let all_combinations = compute_each_combination(&parent_nodes);
+        let mut sum_true = 0f64;
+        let mut sum_false = 0f64;
+        for combination in &all_combinations {
+            let mut product = 1f64;
+            let mut condition = true;
+            for (index, parent_node) in parent_nodes.iter().enumerate() {
+                let boolean_outcome = combination.get(parent_node).unwrap();
+                let usize_outcome = if *boolean_outcome { 1 } else { 0 };
+                print_green!(
+                    "get pi message: parent_node {:?}, node {:?}, outcome: {}",
+                    parent_node,
+                    node,
+                    usize_outcome
+                );
+                let pi_x_z = self
+                    .data
+                    .get_pi_message(parent_node, node, usize_outcome)
+                    .unwrap();
+                print_yellow!(
+                    "boolean_outcome {} usize_outcome {} pi_x_z {}",
+                    boolean_outcome,
+                    usize_outcome,
+                    pi_x_z
+                );
+                product *= pi_x_z;
+                let combination_val = combination[parent_node];
+                condition = condition && combination_val;
+                print_yellow!(
+                    "combination_val {} condition {}",
+                    combination_val,
+                    condition
+                );
+            }
+            if condition {
+                print_blue!("true combination: {:?}, product {}", &combination, product);
+                sum_true += product;
+            } else {
+                print_blue!("false combination: {:?}, product {}", &combination, product);
+                sum_false += product;
+            }
+        }
+        self.data.set_pi_value(node, 1, sum_true);
+        self.data.set_pi_value(node, 0, sum_false);
         Ok(())
     }
 }
