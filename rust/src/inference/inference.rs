cfbf6f12435000ca8f479c8394debbbb7453f471
Greg <greg@hiro.so>
2024-01-18 00:40:18 -0500

    need to fix the debug for proposition node

--- rust/src/inference/inference.rs
+++ rust/src/inference/inference.rs
@@ -111,6 +111,7 @@ impl Inferencer {
 
     pub fn send_pi_messages(&mut self) -> Result<(), Box<dyn Error>> {
         let bfs_order = self.bfs_order.clone();
+        print_red!("send_pi_messages bfs_order: {:?}", &bfs_order);
         for node in &bfs_order {
             print_yellow!("send pi bfs selects {:?}", node);
             self.pi_visit_node(node)?;
