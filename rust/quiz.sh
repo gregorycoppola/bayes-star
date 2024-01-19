605fa83f2e4522852e9b60c140f5219cbdd986b1
Greg <greg@hiro.so>
2024-01-19 04:16:12 -0500

    training on one example!              at /rustc/897e37553bba8b42751c67658967889d11ecd120/library/core/src/panicking.rs:142:14    2: core::panicking::panic              at /rustc/897e37553bba8b42751c67658967889d11ecd120/library/core/src/panicking.rs:48:5    3: core::option::Option<T>::unwrap              at /rustc/897e37553bba8b42751c67658967889d11ecd120/library/core/src/option.rs:775:21    4: bayes_star::inference::lambda::<impl bayes_star::inference::inference::Inferencer>::lambda_send_single              at ./src/inference/lambda.rs:91:34    5: bayes_star::inference::lambda::<impl bayes_star::inference::inference::Inferencer>::lambda_send_generic              at ./src/inference/lambda.rs:69:17    6: bayes_star::inference::lambda::<impl bayes_star::inference::inference::Inferencer>::lambda_visit_node              at ./src/inference/lambda.rs:62:9    7: bayes_star::inference::lambda::<impl bayes_star::inference::inference::Inferencer>::do_lambda_traversal              at ./src/inference/lambda.rs:12:13    8: bayes_star::inference::inference::Inferencer::initialize              at ./src/inference/inference.rs:91:9    9: bayes_star::inference::inference::inference_compute_marginals              at ./src/inference/inference.rs:235:5   10: bayes_star::common::test::inference_example              at ./src/common/test.rs:20:5

--- rust/quiz.sh
+++ rust/quiz.sh
@@ -1,2 +1,2 @@
-RUST_BACKTRACE=1 RUST_LOG=info cargo run --bin test -- --print_training_loss --entities_per_domain=1024 2> output/log
+RUST_BACKTRACE=1 RUST_LOG=info cargo run --bin test -- --print_training_loss --entities_per_domain=1024 --test_example=0 2> output/log
 cat output/log
