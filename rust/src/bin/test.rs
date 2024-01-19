7112af943eb954b4c7ddfcb88b231be11c7307e1
Greg <greg@hiro.so>
2024-01-19 03:29:09 -0500

    checking example

--- rust/src/bin/test.rs
+++ rust/src/bin/test.rs
@@ -1,6 +1,6 @@
 use bayes_star::common::setup::parse_configuration_options;
 use bayes_star::common::resources::FactoryResources;
-use bayes_star::common::test::do_testing;
+use bayes_star::common::test::inference_example;
 
 #[macro_use]
 extern crate log;
@@ -9,6 +9,10 @@ fn main() {
     let config: bayes_star::common::setup::ConfigurationOptions = parse_configuration_options();
     let resources = FactoryResources::new(&config).expect("Couldn't create resources.");
     let test_example = config.test_example;
-    do_testing(&resources).expect("Testing failed.");
-    warn!("program done");
+    if test_example.is_some() {
+        inference_example(&config, &resources).expect("Testing failed.");
+    } else {
+        todo!()
+    }
+    info!("main finishes");
 }
--- rust/src/common/test.rs
+++ rust/src/common/test.rs
@@ -2,9 +2,10 @@ use std::error::Error;
 
 use crate::{common::{graph::InferenceGraph, proposition_db::RedisFactDB, train::TrainingPlan, model::InferenceModel}, model::{exponential::ExponentialModel}, inference::inference::inference_compute_marginals};
 
-use super::resources::FactoryResources;
+use super::{resources::FactoryResources, setup::ConfigurationOptions};
 
-pub fn do_testing(
+pub fn inference_example(
+    config: &ConfigurationOptions,
     resources: &FactoryResources,
 ) -> Result<(), Box<dyn Error>> {
     let plan = TrainingPlan::new(&resources.redis)?;
