281931b2495317fbcec1a999c00f93b52e30ed08
Greg <greg@hiro.so>
2024-01-19 23:32:51 -0500

    hacking.. looks good

--- rust/src/inference/lambda.rs
+++ rust/src/inference/lambda.rs
@@ -82,7 +82,11 @@ impl Inferencer {
 
     pub fn lambda_visit_node(&mut self, from_node: &PropositionNode) -> Result<(), Box<dyn Error>> {
         let is_observed = self.is_observed(from_node)?;
-        print_yellow!("lambda_visit_node {:?} is_observed {}", from_node, is_observed);
+        print_yellow!(
+            "lambda_visit_node {:?} is_observed {}",
+            from_node,
+            is_observed
+        );
         if is_observed {
             self.lambda_set_from_evidence(from_node)?;
         } else {
@@ -95,35 +99,47 @@ impl Inferencer {
     pub fn lambda_send_generic(&mut self, node: &PropositionNode) -> Result<(), Box<dyn Error>> {
         let parent_nodes = self.proposition_graph.get_all_backward(node);
         let all_combinations = compute_each_combination(&parent_nodes);
-        let mut sum_true = 0f64;
-        let mut sum_false = 0f64;
-        for combination in &all_combinations {
-            let mut product = 1f64;
-            for (index, parent_node) in parent_nodes.iter().enumerate() {
-                let boolean_outcome = combination.get(parent_node).unwrap();
-                let usize_outcome = if *boolean_outcome { 1 } else { 0 };
-                let pi_x_z = self
-                    .data
-                    .get_pi_message(parent_node, node, usize_outcome)
-                    .unwrap();
-                trace!(
-                    "getting pi message parent_node {:?}, node {:?}, usize_outcome {}, pi_x_z {}",
-                    &parent_node,
-                    &node,
-                    usize_outcome,
-                    pi_x_z,
-                );
-                product *= pi_x_z;
+        for (to_index, to_parent) in parent_nodes.iter().enumerate() {
+            for combination in &all_combinations {
+                let mut pi_product = 1f64;
+                for (other_index, other_parent) in parent_nodes.iter().enumerate() {
+                    if other_index != to_index {
+                        let this_pi = self.data.get_pi_value(&other_parent, *class_label).unwrap();
+                        pi_product *= this_pi;
+                    }
+                }
             }
-            let true_marginal =
-                self.score_factor_assignment(&parent_nodes, combination, node)?;
-            let false_marginal = 1f64 - true_marginal;
-            let false_marginal = 1f64 - true_marginal;
-            sum_true += true_marginal * product;
-            sum_false += false_marginal * product;
         }
-        self.data.set_lambda_value(node, 1, sum_true);
-        self.data.set_lambda_value(node, 0, sum_false);
+        // let all_combinations = compute_each_combination(&parent_nodes);
+        // let mut sum_true = 0f64;
+        // let mut sum_false = 0f64;
+        // for combination in &all_combinations {
+        //     let mut product = 1f64;
+        //     for (index, parent_node) in parent_nodes.iter().enumerate() {
+        //         let boolean_outcome = combination.get(parent_node).unwrap();
+        //         let usize_outcome = if *boolean_outcome { 1 } else { 0 };
+        //         let pi_x_z = self
+        //             .data
+        //             .get_pi_message(parent_node, node, usize_outcome)
+        //             .unwrap();
+        //         trace!(
+        //             "getting pi message parent_node {:?}, node {:?}, usize_outcome {}, pi_x_z {}",
+        //             &parent_node,
+        //             &node,
+        //             usize_outcome,
+        //             pi_x_z,
+        //         );
+        //         product *= pi_x_z;
+        //     }
+        //     let true_marginal =
+        //         self.score_factor_assignment(&parent_nodes, combination, node)?;
+        //     let false_marginal = 1f64 - true_marginal;
+        //     let false_marginal = 1f64 - true_marginal;
+        //     sum_true += true_marginal * product;
+        //     sum_false += false_marginal * product;
+        // }
+        // self.data.set_lambda_value(node, 1, sum_true);
+        // self.data.set_lambda_value(node, 0, sum_false);
         Ok(())
     }
 }
