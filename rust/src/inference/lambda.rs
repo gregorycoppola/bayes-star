97050d3662ea3fc083016a331d05f3165c27915d
Greg <greg@hiro.so>
2024-01-19 22:47:17 -0500

    this version compiles!.. like this abstraction!

--- rust/src/inference/lambda.rs
+++ rust/src/inference/lambda.rs
@@ -93,22 +93,7 @@ impl Inferencer {
     }
 
     pub fn lambda_send_generic(&mut self, node: &PropositionNode) -> Result<(), Box<dyn Error>> {
-        match &node.node {
-            GenericNodeType::Single(proposition) => {
-                self.lambda_send_single(node)?;
-            }
-            GenericNodeType::Group(group) => {
-                self.lambda_send_group(node)?;
-            }
-        }
-        Ok(())
-    }
-
-    // from_node is a single.. compute it from the group
-    pub fn lambda_send_single(&mut self, node: &PropositionNode) -> Result<(), Box<dyn Error>> {
-        let conclusion = node.extract_single();
         let parent_nodes = self.proposition_graph.get_all_backward(node);
-        let premise_groups = groups_from_backlinks(&parent_nodes);
         let all_combinations = compute_each_combination(&parent_nodes);
         let mut sum_true = 0f64;
         let mut sum_false = 0f64;
@@ -130,72 +115,12 @@ impl Inferencer {
                 );
                 product *= pi_x_z;
             }
-            let factor = build_factor_context_for_assignment(
-                &self.proposition_graph,
-                &premise_groups,
-                combination,
-                &conclusion,
-            );
-            let prediction = self.model.model.predict(&factor)?;
-            print_yellow!(
-                "local probability {}  for factor {:?}",
-                &prediction.marginal,
-                &factor
-            );
-            let true_marginal = &prediction.marginal;
+            let true_marginal =
+                self.score_factor_assignment(&parent_nodes, combination, node)?;
+            let false_marginal = 1f64 - true_marginal;
             let false_marginal = 1f64 - true_marginal;
             sum_true += true_marginal * product;
             sum_false += false_marginal * product;
-        }
-        self.data.set_lambda_value(node, 1, sum_true);
-        self.data.set_lambda_value(node, 0, sum_false);
-        Ok(())
-    }
-
-    pub fn lambda_send_group(&mut self, node: &PropositionNode) -> Result<(), Box<dyn Error>> {
-        let parent_nodes = self.proposition_graph.get_all_backward(node);
-        print_yellow!("lambda_send_group {:?}", &parent_nodes);
-        let all_combinations = compute_each_combination(&parent_nodes);
-        let mut sum_true = 0f64;
-        let mut sum_false = 0f64;
-        for combination in &all_combinations {
-            let mut product = 1f64;
-            let mut condition = true;
-            for (index, parent_node) in parent_nodes.iter().enumerate() {
-                let boolean_outcome = combination.get(parent_node).unwrap();
-                let usize_outcome = if *boolean_outcome { 1 } else { 0 };
-                trace!(
-                    "get pi message: parent_node {:?}, node {:?}, outcome: {}",
-                    parent_node,
-                    node,
-                    usize_outcome
-                );
-                let lambda_x_z = self
-                    .data
-                    .get_pi_message(parent_node, node, usize_outcome)
-                    .unwrap();
-                trace!(
-                    "boolean_outcome {} usize_outcome {} lambda_x_z {}",
-                    boolean_outcome,
-                    usize_outcome,
-                    lambda_x_z
-                );
-                product *= lambda_x_z;
-                let combination_val = combination[parent_node];
-                condition = condition && combination_val;
-                trace!(
-                    "combination_val {} condition {}",
-                    combination_val,
-                    condition
-                );
-            }
-            if condition {
-                print_blue!("true combination: {:?}, product {}", &combination, product);
-                sum_true += product;
-            } else {
-                print_blue!("false combination: {:?}, product {}", &combination, product);
-                sum_false += product;
-            }
         }
         self.data.set_lambda_value(node, 1, sum_true);
         self.data.set_lambda_value(node, 0, sum_false);
