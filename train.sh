cc0ef7d049401dbcac418482b5ea1b6b0806920d
Greg <greg@hiro.so>
2024-01-31 13:46:53 -0500

    remove old scripts

--- train.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd rust
-cargo build
-cd ..
-
-./rust/target/debug/bayes-star
--- trainandanalyze.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Step 1: Build the Rust project
-cd rust
-cargo build
-cd ..
-
-# Step 2 and 3: Run the main Rust program and pipe its output to the Python script
-./rust/target/debug/bayes-star | python3 python/analyze.py
