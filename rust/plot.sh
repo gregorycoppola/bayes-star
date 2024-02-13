6a1f329c474fb0076f681a1dd3a97c845caf92d0
Greg <greg@hiro.so>
2024-02-12 22:04:23 -0500

    call plot

--- rust/plot.sh
+++ rust/plot.sh
@@ -11,7 +11,9 @@ if [ -z "$ROOT_DATA" ] || [ -z "$SCENARIO_NAME" ] || [ -z "$TEST_SCENARIO" ]; th
 fi
 
 # Compute MARGINAL_OUTPUT_FILE based on SCENARIO_NAME and TEST_SCENARIO
-MARGINAL_OUTPUT_FILE="${ROOT_DATA}/${SCENARIO_NAME}_${TEST_SCENARIO}"
+MARGINAL_OUTPUT_FILE="${ROOT_DATA}/${SCENARIO_NAME}_${TEST_SCENARIO}.data"
 
 # Proceed with the original command using the variables and add the --marginal_output_file argument
-RUST_BACKTRACE=1 RUST_LOG=info cargo run --bin plot -- --print_training_loss --entities_per_domain=1024 --test_example=0 --scenario_name=$SCENARIO_NAME --test_scenario=$TEST_SCENARIO --marginal_output_file=$MARGINAL_OUTPUT_FILE
+# RUST_BACKTRACE=1 RUST_LOG=info cargo run --bin plot -- --print_training_loss --entities_per_domain=1024 --test_example=0 --scenario_name=$SCENARIO_NAME --test_scenario=$TEST_SCENARIO --marginal_output_file=$MARGINAL_OUTPUT_FILE
+
+python3 ../python/plotmarginals.py
