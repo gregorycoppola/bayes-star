3ad2afe618851041330a1802170b22c852031924
Greg <greg@hiro.so>
2025-01-07 07:43:26 -0500

    less errors

--- rust/src/scenarios/two_var.rs
+++ rust/src/scenarios/two_var.rs
@@ -29,7 +29,7 @@ impl ScenarioMaker for TwoVariable {
     ) -> Result<(), Box<dyn Error>> {
         let mut graph = InferenceGraph::new_mutable(resources)?;
         let proposition_db = RedisBeliefTable::new_mutable(&resources.redis)?;
-        let mut plan = TrainingPlan::new(&resources.redis)?;
+        let mut plan = TrainingPlan::new(&resources)?;
         let config = &resources.config;
         let total_members_each_class = config.entities_per_domain;
         let jack_domain = Domain::Man;
