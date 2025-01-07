f41861f838d23cd4dd74c832eda0753b7c8b7cb4
Greg <greg@hiro.so>
2025-01-07 07:23:35 -0500

    comment out methods not used

--- rust/src/common/redis.rs
+++ rust/src/common/redis.rs
@@ -25,24 +25,24 @@ impl RedisManager {
     }
 }
 
-pub fn map_insert(
-    conn: &mut Connection,
-    key: &str,
-    field: &str,
-    value: &str,
-) -> Result<(), Box<dyn Error>> {
-    conn.hset(key, field, value)?;
-    Ok(())
-}
+// pub fn map_insert(
+//     conn: &mut Connection,
+//     key: &str,
+//     field: &str,
+//     value: &str,
+// ) -> Result<(), Box<dyn Error>> {
+//     conn.hset(key, field, value)?;
+//     Ok(())
+// }
 
-pub fn map_get(
-    conn: &mut Connection,
-    key: &str,
-    field: &str,
-) -> Result<Option<String>, Box<dyn Error>> {
-    let value: Option<String> = conn.hget(key, field)?;
-    Ok(value)
-}
+// pub fn map_get(
+//     conn: &mut Connection,
+//     key: &str,
+//     field: &str,
+// ) -> Result<Option<String>, Box<dyn Error>> {
+//     let value: Option<String> = conn.hget(key, field)?;
+//     Ok(value)
+// }
 
 pub fn set_add(conn: &mut Connection, key: &str, member: &str) -> Result<bool, Box<dyn Error>> {
     let added: bool = conn.sadd(key, member)?;
@@ -54,15 +54,15 @@ pub fn set_members(conn: &mut Connection, key: &str) -> Result<Vec<String>, Box<
     Ok(members)
 }
 
-pub fn seq_push(conn: &mut Connection, key: &str, value: &str) -> Result<i64, Box<dyn Error>> {
-    let length: i64 = conn.rpush(key, value)?;
-    Ok(length)
-}
+// pub fn seq_push(conn: &mut Connection, key: &str, value: &str) -> Result<i64, Box<dyn Error>> {
+//     let length: i64 = conn.rpush(key, value)?;
+//     Ok(length)
+// }
 
-pub fn seq_pop(conn: &mut Connection, key: &str) -> Result<Option<String>, Box<dyn Error>> {
-    let value: Option<String> = conn.lpop(key, None)?;
-    Ok(value)
-}
+// pub fn seq_pop(conn: &mut Connection, key: &str) -> Result<Option<String>, Box<dyn Error>> {
+//     let value: Option<String> = conn.lpop(key, None)?;
+//     Ok(value)
+// }
 
 pub fn seq_get_all(conn: &mut Connection, key: &str) -> Result<Vec<String>, Box<dyn Error>> {
     let elements: Vec<String> = conn.lrange(key, 0, -1)?;
