8bf2dbf3c97c6e2537982af14657167c476bb2d1
Greg <greg@hiro.so>
2025-01-08 09:41:20 -0500

    safety bug. not sure

--- rust/src/bin/explorer_server.rs
+++ rust/src/bin/explorer_server.rs
@@ -2,27 +2,42 @@
 extern crate rocket;
 
 use rocket::State;
+use bayes_star::common::{graph::InferenceGraph, resources::FactoryResources, setup::parse_configuration_options};
 
-struct RedisManager {
-    pool: redis::Client,
+struct AppContext {
+    graph: InferenceGraph,
 }
 
-impl RedisManager {
-    pub async fn new(connection_string: &str) -> Self {
-        let client =
-            redis::Client::open(connection_string).expect("Redis client initialization failed");
-        RedisManager { pool: client }
+impl AppContext {
+    pub fn new() -> Self {
+        let config = parse_configuration_options();
+        let resources = FactoryResources::new(&config).expect("Failed to create factory resources");
+        let graph = InferenceGraph::new_shared(&resources).expect("Failed to create inference graph");
+        AppContext { graph }
     }
 }
 
-#[get("/")]
-fn index(redis: &State<RedisManager>) -> &'static str {
-    "Hello, world!"
+#[get("/domains")]
+fn domains(context: &State<AppContext>) -> String {
+    let all_domains = context.graph.get_all_domains().expect("Failed to get domains");
+    format!("{:?}", all_domains)
+}
+
+#[get("/relations")]
+fn relations(context: &State<AppContext>) -> String {
+    let all_relations = context.graph.get_all_relations().expect("Failed to get relations");
+    format!("{:?}", all_relations)
+}
+
+#[get("/implications")]
+fn implications(context: &State<AppContext>) -> String {
+    let all_implications = context.graph.get_all_implications().expect("Failed to get implications");
+    format!("{:?}", all_implications)
 }
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-        .manage(RedisManager::new("redis://127.0.0.1/"))
-        .mount("/", routes![index])
+        .manage(AppContext::new())
+        .mount("/", routes![domains, relations, implications])
 }
