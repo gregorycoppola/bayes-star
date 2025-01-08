fafc66004821380801a957b3881528a62ed01798
Greg <greg@hiro.so>
2025-01-07 20:15:39 -0500

    added graph

--- rust/src/bin/list_entities.rs
+++ rust/src/bin/list_entities.rs
@@ -1,8 +1,9 @@
-use bayes_star::common::{resources::FactoryResources, setup::parse_configuration_options};
+use bayes_star::common::{graph::InferenceGraph, resources::FactoryResources, setup::parse_configuration_options};
 
 fn main() {
     let config: bayes_star::common::setup::ConfigurationOptions = parse_configuration_options();
     let resources = FactoryResources::new(&config).expect("Couldn't create resources.");
+    let mut graph = InferenceGraph::new_mutable(&resources).expect("");
     println!("main finishes");
 }
 
