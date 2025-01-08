f140818a0a3c7c6e0e3fafa626960f93e7a0615a
Greg <greg@hiro.so>
2025-01-08 16:44:49 -0500

    fixing the basic predicates and stuff

--- rust/src/explorer/diagram_utils.rs
+++ rust/src/explorer/diagram_utils.rs
@@ -1,32 +1,49 @@
 use crate::model::objects::{ImplicationFactor, Predicate, PredicateGroup, Relation};
 
 fn diagram_domain(domain: &String) -> String {
-    format!(r#"
+    format!(
+        r#"
                 <span class='domain_span'>
                     <span class='domain_label'>{domain}</span>
                     <span><img src='/static/images/domains/{domain}.png' class='domain_icon' /></span>
                 </span>
-    "#)
+    "#
+    )
 }
 
 fn diagram_relation(relation: &Relation) -> String {
-    let types_html = relation.types.iter()
-        .map(|var_arg| format!("<span class='type'>{}</span>", var_arg)) // Assuming var_arg can be displayed
-        .collect::<Vec<_>>()
-        .join(", ");
-    format!("<div class='relation'><strong>{}</strong>: [{}]</div>", relation.relation_name, types_html)
+    format!(
+        r#"
+        <span class='relation'>
+            <span class='relation_name'>
+                {relation_name}
+            </span>
+        </span>
+    "#,
+        relation_name = &relation.relation_name
+    )
 }
 
 fn diagram_predicate(predicate: &Predicate) -> String {
     let relation_html = diagram_relation(&predicate.relation);
-    let roles_html = predicate.roles.iter()
-        .map(|role| format!("<span class='labeled_argument'><strong>{}</strong>: {}</span>", role.role_name, role.argument.hash_string())) // Assuming argument can display hash_string
+    let roles_html = predicate
+        .roles
+        .iter()
+        .map(|role| {
+            format!(
+                "<span class='labeled_argument'><strong>{}</strong>: {}</span>",
+                role.role_name,
+                role.argument.hash_string()
+            )
+        }) // Assuming argument can display hash_string
         .collect::<Vec<_>>()
         .join(", ");
 
-    format!("<div class='predicate'>{}<div class='roles'>[{}]</div></div>", relation_html, roles_html)
+    format!(
+        "<div class='predicate'>{}<div class='roles'>[{}]</div></div>",
+        relation_html, roles_html
+    )
 }
-
 
 fn diagram_predicate_group(group: &PredicateGroup) -> String {
     let mut parts = vec![];
