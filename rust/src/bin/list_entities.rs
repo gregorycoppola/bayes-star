ae4bb12bb1a01be05a4f7b03622700a72757d1bd
Greg <greg@hiro.so>
2025-01-07 20:23:50 -0500

    it's listing the domains

--- rust/src/bin/list_entities.rs
+++ rust/src/bin/list_entities.rs
@@ -4,6 +4,9 @@ fn main() {
     let config: bayes_star::common::setup::ConfigurationOptions = parse_configuration_options();
     let resources = FactoryResources::new(&config).expect("Couldn't create resources.");
     let graph = InferenceGraph::new_shared(&resources).expect("");
+    let all_domains = graph.get_all_domains().unwrap();
+
+    println!("all_domains {:?}", &all_domains);
     println!("main finishes");
 }
 
