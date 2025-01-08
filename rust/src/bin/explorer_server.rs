8f8b92ace100fc40adab3d439ff72eecfce99216
Greg <greg@hiro.so>
2025-01-08 09:30:36 -0500

    nice

--- rust/src/bin/explorer_server.rs
+++ rust/src/bin/explorer_server.rs
@@ -1,4 +1,19 @@
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
+
+use rocket::State;
+
+struct RedisManager {
+    pool: redis::Client,
+}
+
+impl RedisManager {
+    pub async fn new(connection_string: &str) -> Self {
+        let client =
+            redis::Client::open(connection_string).expect("Redis client initialization failed");
+        RedisManager { pool: client }
+    }
+}
 
 #[get("/")]
 fn index() -> &'static str {
@@ -7,5 +22,7 @@ fn index() -> &'static str {
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build()
+        .manage(RedisManager::new("redis://127.0.0.1/"))
+        .mount("/", routes![index])
 }
