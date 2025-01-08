3ad9f6df558b378fa3b22871c29c1a1c226beb43
Greg <greg@hiro.so>
2025-01-08 04:56:24 -0500

    add relations to the printout thing

--- rust/src/bin/list_entities.rs
+++ rust/src/bin/list_entities.rs
@@ -2,15 +2,24 @@ use bayes_star::common::{graph::InferenceGraph, resources::FactoryResources, set
 
 fn main() {
     let config: bayes_star::common::setup::ConfigurationOptions = parse_configuration_options();
-    let resources = FactoryResources::new(&config).expect("Couldn't create resources.");
-    let graph = InferenceGraph::new_shared(&resources).expect("");
+    let resources = FactoryResources::new(&config).unwrap();
+    let graph = InferenceGraph::new_shared(&resources).unwrap();
+    //
+    // Domains.
     let all_domains = graph.get_all_domains().unwrap();
     println!("all_domains {:?}", &all_domains);
-
     for domain in &all_domains {
         let elements = graph.get_entities_in_domain(domain).unwrap();
         println!("elements: {:?}", &elements);
     }
+    //
+    // Relations.
+    let all_relations = graph.get_all_relations().unwrap();
+    println!("all_relations {:?}", &all_relations);
+    for relation in &all_relations {
+        println!("relation {:?}", relation);
+    }
+
     println!("main finishes");
 }
 
