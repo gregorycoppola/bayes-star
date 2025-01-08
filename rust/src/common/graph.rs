dc1b3950591ebd1333c43c6c41479d1eb6d1c15d
Greg <greg@hiro.so>
2025-01-08 03:44:23 -0500

    nice

--- rust/src/common/graph.rs
+++ rust/src/common/graph.rs
@@ -162,6 +162,7 @@ impl InferenceGraph {
         self.store_predicate_backward_link(implication)?;
         Ok(())
     }
+
     pub fn store_predicate_implications(
         &mut self,
         implications: &Vec<PredicateFactor>,
@@ -171,6 +172,7 @@ impl InferenceGraph {
         }
         Ok(())
     }
+
     pub fn get_all_implications(&self) -> Result<Vec<PredicateFactor>, Box<dyn Error>> {
         let set_members: Vec<String> = set_members(
             &mut *self.redis_connection.borrow_mut(),
