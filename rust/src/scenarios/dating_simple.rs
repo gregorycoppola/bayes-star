81aadcf709f1fd26ddee96190b2ef8faf9d60d9e
Greg <greg@hiro.so>
2025-01-08 16:42:43 -0500

    it's messed up but close

--- rust/src/scenarios/dating_simple.rs
+++ rust/src/scenarios/dating_simple.rs
@@ -95,121 +95,121 @@ impl ScenarioMaker for SimpleDating {
         );
         graph.register_relation(&jack_date_jill_relation)?;
 
-        for i in 0..total_members_each_class {
-            let is_test = i == 0;
-            let is_training = !is_test;
-            let mut domain_entity_map: HashMap<String, Entity> = HashMap::new();
-            for domain in entity_domains.iter() {
-                let prefix = if is_test { "test" } else { "train" };
-                let name = format!("{}_{:?}{}", &prefix, domain, i); // Using Debug formatting for Domain enum
-                let entity = Entity {
-                    domain: domain.clone(),
-                    name: name.clone(),
-                };
-                graph.store_entity(&entity)?;
-                trace!("Stored entity: {:?}", &entity);
-                domain_entity_map.insert(domain.to_string(), entity);
-            }
+        // for i in 0..total_members_each_class {
+        //     let is_test = i == 0;
+        //     let is_training = !is_test;
+        //     let mut domain_entity_map: HashMap<String, Entity> = HashMap::new();
+        //     for domain in entity_domains.iter() {
+        //         let prefix = if is_test { "test" } else { "train" };
+        //         let name = format!("{}_{:?}{}", &prefix, domain, i); // Using Debug formatting for Domain enum
+        //         let entity = Entity {
+        //             domain: domain.clone(),
+        //             name: name.clone(),
+        //         };
+        //         graph.store_entity(&entity)?;
+        //         trace!("Stored entity: {:?}", &entity);
+        //         domain_entity_map.insert(domain.to_string(), entity);
+        //     }
 
-            let jack_entity = &domain_entity_map[&Domain::MAN.to_string()];
-            let jill_entity = &domain_entity_map[&Domain::WOMAN.to_string()];
+        //     let jack_entity = &domain_entity_map[&Domain::MAN.to_string()];
+        //     let jill_entity = &domain_entity_map[&Domain::WOMAN.to_string()];
 
-            let p_jack_lonely = weighted_cointoss(0.3f64);
-            let p_jill_exciting: f64 = weighted_cointoss(0.6f64);
-            let p_jill_likes_jack: f64 = weighted_cointoss(0.4f64);
-            let p_jack_likes_jill = weighted_cointoss(numeric_or(p_jack_lonely, p_jill_exciting));
-            let p_jack_dates_jill = numeric_and(p_jack_likes_jill, p_jill_likes_jack);
+        //     let p_jack_lonely = weighted_cointoss(0.3f64);
+        //     let p_jill_exciting: f64 = weighted_cointoss(0.6f64);
+        //     let p_jill_likes_jack: f64 = weighted_cointoss(0.4f64);
+        //     let p_jack_likes_jill = weighted_cointoss(numeric_or(p_jack_lonely, p_jill_exciting));
+        //     let p_jack_dates_jill = numeric_and(p_jack_likes_jill, p_jill_likes_jack);
 
-            {
-                trace!("Man entity part 2: {:?}", jack_entity);
-                let jack = constant(jack_entity.domain.clone(), jack_entity.name.clone());
-                let jack_lonely = proposition(lonely_jack_relation.clone(), vec![sub(jack)]);
+        //     {
+        //         trace!("Man entity part 2: {:?}", jack_entity);
+        //         let jack = constant(jack_entity.domain.clone(), jack_entity.name.clone());
+        //         let jack_lonely = proposition(lonely_jack_relation.clone(), vec![sub(jack)]);
 
-                trace!(
-                    "Man Lonely: {:?}, Probability: {}",
-                    jack_lonely.predicate.hash_string(),
-                    p_jack_lonely
-                );
-                proposition_db.store_proposition_probability(&jack_lonely, p_jack_lonely)?;
-                plan.maybe_add_to_training(is_training, &jack_lonely)?;
-                graph.ensure_existence_backlinks_for_proposition(&jack_lonely)?;
-            }
+        //         trace!(
+        //             "Man Lonely: {:?}, Probability: {}",
+        //             jack_lonely.predicate.hash_string(),
+        //             p_jack_lonely
+        //         );
+        //         proposition_db.store_proposition_probability(&jack_lonely, p_jack_lonely)?;
+        //         plan.maybe_add_to_training(is_training, &jack_lonely)?;
+        //         graph.ensure_existence_backlinks_for_proposition(&jack_lonely)?;
+        //     }
 
-            {
-                let jill = constant(jill_entity.domain.clone(), jill_entity.name.clone());
-                let jill_exciting = proposition(exciting_jill_relation.clone(), vec![sub(jill)]);
+        //     {
+        //         let jill = constant(jill_entity.domain.clone(), jill_entity.name.clone());
+        //         let jill_exciting = proposition(exciting_jill_relation.clone(), vec![sub(jill)]);
 
-                trace!(
-                    "Woman Exciting: {:?}, Probability: {}",
-                    jill_exciting.predicate.hash_string(),
-                    p_jill_exciting
-                );
-                proposition_db.store_proposition_probability(&jill_exciting, p_jill_exciting)?;
-                plan.maybe_add_to_training(is_training, &jill_exciting)?;
-                graph.ensure_existence_backlinks_for_proposition(&jill_exciting)?;
-            }
+        //         trace!(
+        //             "Woman Exciting: {:?}, Probability: {}",
+        //             jill_exciting.predicate.hash_string(),
+        //             p_jill_exciting
+        //         );
+        //         proposition_db.store_proposition_probability(&jill_exciting, p_jill_exciting)?;
+        //         plan.maybe_add_to_training(is_training, &jill_exciting)?;
+        //         graph.ensure_existence_backlinks_for_proposition(&jill_exciting)?;
+        //     }
 
-            {
-                let jill = constant(jill_entity.domain.clone(), jill_entity.name.clone());
-                let jack = constant(jack_entity.domain.clone(), jack_entity.name.clone());
+        //     {
+        //         let jill = constant(jill_entity.domain.clone(), jill_entity.name.clone());
+        //         let jack = constant(jack_entity.domain.clone(), jack_entity.name.clone());
 
-                // "likes(jill, jack)"
-                let jill_likes_jack = proposition(
-                    jill_like_jack_relation.clone(),
-                    vec![sub(jill.clone()), obj(jack.clone())],
-                );
-                trace!(
-                    "Woman likes Man: {:?}, Probability: {}",
-                    jill_likes_jack.predicate.hash_string(),
-                    p_jill_likes_jack
-                ); // Logging
-                proposition_db
-                    .store_proposition_probability(&jill_likes_jack, p_jill_likes_jack)?;
-                plan.maybe_add_to_training(is_training, &jill_likes_jack)?;
-                graph.ensure_existence_backlinks_for_proposition(&jill_likes_jack)?;
-            }
+        //         // "likes(jill, jack)"
+        //         let jill_likes_jack = proposition(
+        //             jill_like_jack_relation.clone(),
+        //             vec![sub(jill.clone()), obj(jack.clone())],
+        //         );
+        //         trace!(
+        //             "Woman likes Man: {:?}, Probability: {}",
+        //             jill_likes_jack.predicate.hash_string(),
+        //             p_jill_likes_jack
+        //         ); // Logging
+        //         proposition_db
+        //             .store_proposition_probability(&jill_likes_jack, p_jill_likes_jack)?;
+        //         plan.maybe_add_to_training(is_training, &jill_likes_jack)?;
+        //         graph.ensure_existence_backlinks_for_proposition(&jill_likes_jack)?;
+        //     }
 
-            {
-                let jill = constant(jill_entity.domain.clone(), jill_entity.name.clone());
-                let jack = constant(jack_entity.domain.clone(), jack_entity.name.clone());
-                let jack_likes_jill = proposition(
-                    jack_like_jill_relation.clone(),
-                    vec![sub(jack.clone()), obj(jill.clone())],
-                );
-                trace!(
-                    "Man likes Woman: {:?}, Probability: {}",
-                    jack_likes_jill.predicate.hash_string(),
-                    p_jack_likes_jill
-                ); // Logging
-                if is_training {
-                    proposition_db
-                        .store_proposition_probability(&jack_likes_jill, p_jack_likes_jill)?;
-                }
-                plan.maybe_add_to_training(is_training, &jack_likes_jill)?;
-                // graph.ensure_existence_backlinks_for_proposition(&jack_likes_jill)?;
-            }
-            {
-                let jill = constant(jill_entity.domain.clone(), jill_entity.name.clone());
-                let jack = constant(jack_entity.domain.clone(), jack_entity.name.clone());
+        //     {
+        //         let jill = constant(jill_entity.domain.clone(), jill_entity.name.clone());
+        //         let jack = constant(jack_entity.domain.clone(), jack_entity.name.clone());
+        //         let jack_likes_jill = proposition(
+        //             jack_like_jill_relation.clone(),
+        //             vec![sub(jack.clone()), obj(jill.clone())],
+        //         );
+        //         trace!(
+        //             "Man likes Woman: {:?}, Probability: {}",
+        //             jack_likes_jill.predicate.hash_string(),
+        //             p_jack_likes_jill
+        //         ); // Logging
+        //         if is_training {
+        //             proposition_db
+        //                 .store_proposition_probability(&jack_likes_jill, p_jack_likes_jill)?;
+        //         }
+        //         plan.maybe_add_to_training(is_training, &jack_likes_jill)?;
+        //         // graph.ensure_existence_backlinks_for_proposition(&jack_likes_jill)?;
+        //     }
+        //     {
+        //         let jill = constant(jill_entity.domain.clone(), jill_entity.name.clone());
+        //         let jack = constant(jack_entity.domain.clone(), jack_entity.name.clone());
 
-                // "dates(jack, jill)" based on "likes(jack, jill) and likes(jill, jack)"
-                let jack_dates_jill =
-                    proposition(jack_date_jill_relation.clone(), vec![sub(jack), obj(jill)]);
-                trace!(
-                    "Man dates Woman: {:?}, Probability: {}",
-                    jack_dates_jill.predicate.hash_string(),
-                    p_jack_dates_jill
-                ); // Logging
+        //         // "dates(jack, jill)" based on "likes(jack, jill) and likes(jill, jack)"
+        //         let jack_dates_jill =
+        //             proposition(jack_date_jill_relation.clone(), vec![sub(jack), obj(jill)]);
+        //         trace!(
+        //             "Man dates Woman: {:?}, Probability: {}",
+        //             jack_dates_jill.predicate.hash_string(),
+        //             p_jack_dates_jill
+        //         ); // Logging
 
-                if is_training {
-                    proposition_db
-                        .store_proposition_probability(&jack_dates_jill, p_jack_dates_jill)?;
-                }
-                plan.maybe_add_to_training(is_training, &jack_dates_jill)?;
-                plan.maybe_add_to_test(is_test, &jack_dates_jill)?;
-                // graph.ensure_existence_backlinks_for_proposition(&jack_dates_jill)?;
-            }
-        }
+        //         if is_training {
+        //             proposition_db
+        //                 .store_proposition_probability(&jack_dates_jill, p_jack_dates_jill)?;
+        //         }
+        //         plan.maybe_add_to_training(is_training, &jack_dates_jill)?;
+        //         plan.maybe_add_to_test(is_test, &jack_dates_jill)?;
+        //         // graph.ensure_existence_backlinks_for_proposition(&jack_dates_jill)?;
+        //     }
+        // }
 
         let xjack = variable(Domain::MAN.to_string());
         let xjill = variable(Domain::WOMAN.to_string());
