3033b459e868a5424f79c1fd4761de5e52d71db7
Greg <greg@hiro.so>
2025-01-08 18:24:42 -0500

    need to hack the "configuration options" deeper into things
    there's still kind of a bug where we are getting different parts of the input from inconsistent places but oh well

--- rust/src/explorer/routes/network_route.rs
+++ rust/src/explorer/routes/network_route.rs
@@ -1,14 +1,30 @@
+use std::error::Error;
+
 use rocket::response::content::Html;
 
-use crate::{common::graph::InferenceGraph, explorer::render_utils::render_app_body};
+use crate::{common::{graph::InferenceGraph, model::InferenceModel, proposition_db::EmptyBeliefTable, resources::FactoryResources, train::TrainingPlan}, explorer::render_utils::render_app_body, inference::{graph::PropositionGraph, inference::Inferencer}};
 
+fn get_resources() -> FactoryResources {
+    todo!()
+}
 
-fn render_network(graph: &InferenceGraph) -> String {
-    "".to_string()
+fn render_network(graph: &InferenceGraph) -> Result<String, Box<dyn Error>> {
+    let resources = get_resources();
+    let plan = TrainingPlan::new(&resources)?;
+    let model = InferenceModel::new_shared(&resources).unwrap();
+    let test_questions = plan.get_test_questions().unwrap();
+    let target = &test_questions[0];
+    let fact_memory = EmptyBeliefTable::new_shared(&resources.redis)?;
+    let proposition_graph = PropositionGraph::new_shared(model.graph.clone(), target)?;
+    proposition_graph.visualize();
+    let mut inferencer =
+        Inferencer::new_mutable(&config, model.clone(), proposition_graph.clone(), fact_memory)?;
+    inferencer.initialize_chart()?;
+    Ok("todo".to_string())
 }
 
 pub fn internal_network(graph: &InferenceGraph) -> Html<String> {
-    let network = render_network(graph);
+    let network = render_network(graph).unwrap();
     let body_html = format!(
         r#"
         {network}
