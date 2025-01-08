c4c2781bb9911e7ac13fa94705ce1c8aa98263c2
Greg <greg@hiro.so>
2025-01-08 18:15:38 -0500

    pipe in the "inference graph"

--- rust/src/explorer/routes/network_route.rs
+++ rust/src/explorer/routes/network_route.rs
@@ -1,9 +1,19 @@
 use rocket::response::content::Html;
 
-use crate::explorer::render_utils::render_app_body;
+use crate::{common::graph::InferenceGraph, explorer::render_utils::render_app_body};
 
 
-pub fn internal_index() -> Html<String> {
-    let result = render_app_body("");
+fn render_network(graph: &InferenceGraph) -> String {
+    "".to_string()
+}
+
+pub fn internal_network(graph: &InferenceGraph) -> Html<String> {
+    let network = render_network(graph);
+    let body_html = format!(
+        r#"
+        {network}
+    "#,
+    );
+    let result = render_app_body(&body_html);
     Html(result.unwrap())
 }
