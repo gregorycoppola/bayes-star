65bd37071e369f1d56338fda8ab8e60c73b668cf
Greg <greg@coppola.ai>
2025-01-09 18:01:12 -0500

    train not working now

--- rust/src/bin/list_entities.rs
+++ rust/src/bin/list_entities.rs
@@ -3,26 +3,26 @@ use bayes_star::common::{graph::InferenceGraph, resources::ResourceContext, setu
 fn main() {
     let config: bayes_star::common::setup::CommandLineOptions = parse_configuration_options();
     let resources = ResourceContext::new(&config).unwrap();
-    // let graph = InferenceGraph::new_shared(&resources).unwrap();
-    let graph = InferenceGraph::new_shared(resources.connection.clone(), resources.namespace.clone()).unwrap();
+    let mut connection = resources.connection.lock().unwrap();
+    let graph = InferenceGraph::new_shared(config.scenario_name.clone()).unwrap();
     //
     // Domains.
-    let all_domains = graph.get_all_domains().unwrap();
+    let all_domains = graph.get_all_domains(&mut connection).unwrap();
     println!("all_domains {:?}", &all_domains);
     for domain in &all_domains {
-        let elements = graph.get_entities_in_domain(domain).unwrap();
+        let elements = graph.get_entities_in_domain(&mut connection, domain).unwrap();
         println!("elements: {:?}", &elements);
     }
     //
     // Relations.
-    let all_relations = graph.get_all_relations().unwrap();
+    let all_relations = graph.get_all_relations(&mut connection).unwrap();
     println!("all_relations {:?}", &all_relations);
     for relation in &all_relations {
         println!("relation {:?}", relation);
     }
     //
     // Implications.
-    let all_implications = graph.get_all_implications().unwrap();
+    let all_implications = graph.get_all_implications(&mut connection).unwrap();
     println!("all_implications {:?}", &all_implications);
     for implication in &all_implications {
         println!("implication {:?}", implication);
