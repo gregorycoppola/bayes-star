50654b63ff5a312ba642866ea2a70d4bcbab4f5f
Greg <greg@coppola.ai>
2025-01-09 10:29:19 -0500

    make prop graph

--- rust/src/explorer/routes/network_route.rs
+++ rust/src/explorer/routes/network_route.rs
@@ -10,7 +10,7 @@ fn get_resources() -> NamespaceBundle {
 
 fn render_network(namespace: &NamespaceBundle) -> Result<String, Box<dyn Error>> {
     let graph = InferenceGraph::new_shared(namespace.connection.clone(), namespace.namespace.clone())?;
-    // let proposition_graph = PropositionGraph::new_shared(graph)?;
+    let proposition_graph = PropositionGraph::new_shared(&graph)?;
     // // proposition_graph.visualize();
     // let mut inferencer =
     //     Inferencer::new_mutable(config, model.clone(), proposition_graph.clone(), fact_memory)?;
