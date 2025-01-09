b7e77ab4e30224311b91c27bd380c3683e056ea7
Greg <greg@hiro.so>
2025-01-09 08:55:29 -0500

    new function

--- rust/src/common/resources.rs
+++ rust/src/common/resources.rs
@@ -20,3 +20,15 @@ pub struct NamespaceBundle {
     pub namespace: String,
     pub connection: Arc<Mutex<redis::Connection>>,
 }
+
+impl NamespaceBundle {
+    pub fn new_from_cli(options: &CommandLineOptions) -> Result<NamespaceBundle, Box<dyn Error>> {
+        let namespace = options.scenario_name.clone();
+        let manager = RedisManager::new()?;
+        let connection = manager.get_arc_mutex_guarded_connection()?;
+        Ok(NamespaceBundle {
+            namespace,
+            connection,
+        })
+    }
+}
