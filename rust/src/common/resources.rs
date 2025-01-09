c14f868a5d36d26fa971a7bfe98876a57fc1278f
Greg <greg@coppola.ai>
2025-01-09 16:21:58 -0500

    removve namespace from the rsource bundle thing

--- rust/src/common/resources.rs
+++ rust/src/common/resources.rs
@@ -2,17 +2,14 @@ use std::{error::Error, sync::{Arc, Mutex}};
 use super::{redis::RedisManager, setup::CommandLineOptions};
 
 pub struct ResourceBundle {
-    pub namespace: String,
     pub connection: Arc<Mutex<redis::Connection>>,
 }
 
 impl ResourceBundle {
     pub fn new(options: &CommandLineOptions) -> Result<ResourceBundle, Box<dyn Error>> {
-        let namespace = options.scenario_name.clone();
         let manager = RedisManager::new()?;
         let connection = manager.get_arc_mutex_guarded_connection()?;
         Ok(ResourceBundle {
-            namespace,
             connection,
         })
     }
