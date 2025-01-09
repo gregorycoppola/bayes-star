c1541897d0cc8af7b4ee3dd3261ad8a194e0e344
Greg <greg@hiro.so>
2025-01-09 06:57:44 -0500

    not working

--- rust/src/bin/explorer_server.rs
+++ rust/src/bin/explorer_server.rs
@@ -1,11 +1,23 @@
 use rocket::launch;
 use rocket::{get, routes, Rocket, Build};
+use rocket::{response::{self, Response}, http::ContentType};
+use std::io::Cursor;
+// // Dynamically created HTML response
+// #[get("/")]
+// fn index() -> String {
+//     let greeting = "Hello, world!";
+//     // format!("<h1>{}</h1>", greeting)
+//     format!("<!DOCTYPE html><html><head><title>Home Page</title></head><body><h1>{}</h1></body></html>", greeting)
+// }
 
-// Dynamically created HTML response
 #[get("/")]
-fn index() -> String {
+fn index() -> response::Result<'static> {
     let greeting = "Hello, world!";
-    format!("<h1>{}</h1>", greeting)
+    let html_content = format!("<!DOCTYPE html><html><head><title>Home Page</title></head><body><h1>{}</h1></body></html>", greeting);
+    Response::build()
+        .header(ContentType::HTML)
+        .sized_body(None, Cursor::new(html_content))
+        .ok()
 }
 
 // Another example of dynamically created HTML
