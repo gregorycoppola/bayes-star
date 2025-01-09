a22409c692e46012cbe17d18cdc0ab630e66cfe6
Greg <greg@coppola.ai>
2025-01-09 17:38:58 -0500

    network route works

--- rust/src/explorer/routes/network_route.rs
+++ rust/src/explorer/routes/network_route.rs
@@ -1,46 +1,81 @@
 use std::{error::Error, rc::Rc};
 
+use redis::Connection;
 use rocket::response::content::Html;
 
-use crate::{common::{graph::InferenceGraph, model::InferenceModel, proposition_db::EmptyBeliefTable, resources::ResourceContext, setup::CommandLineOptions, train::TrainingPlan}, explorer::{diagram_utils::{diagram_implication, diagram_predicate, diagram_proposition_factor}, render_utils::render_app_body}, inference::{graph::PropositionGraph, inference::Inferencer, table::PropositionNode}, model::{choose::extract_backimplications_from_proposition, objects::{Proposition, PropositionGroup}}};
+use crate::{
+    common::{
+        graph::InferenceGraph, model::InferenceModel, proposition_db::EmptyBeliefTable,
+        resources::ResourceContext, setup::CommandLineOptions, train::TrainingPlan,
+    },
+    explorer::{
+        diagram_utils::{diagram_implication, diagram_predicate, diagram_proposition_factor},
+        render_utils::render_app_body,
+    },
+    inference::{graph::PropositionGraph, inference::Inferencer, table::PropositionNode},
+    model::{
+        choose::extract_backimplications_from_proposition,
+        objects::{Proposition, PropositionGroup},
+    },
+};
 
 fn get_resources() -> ResourceContext {
     todo!()
 }
 
-fn backwards_print_group(inferencer: &Inferencer, target: &PropositionGroup) -> Result<String, Box<dyn Error>> {
+fn backwards_print_group(
+    connection: &mut Connection,
+    inferencer: &Inferencer,
+    target: &PropositionGroup,
+) -> Result<String, Box<dyn Error>> {
     let proposition_node = PropositionNode::from_group(&target);
-    let backlinks = inferencer.proposition_graph.get_all_backward(&proposition_node);
+    let backlinks = inferencer
+        .proposition_graph
+        .get_all_backward(&proposition_node);
     let mut buffer = "".to_string();
     for backlink in &backlinks {
         let single = backlink.extract_single();
-        let part = backwards_print_single(inferencer, &single)?;
+        let part = backwards_print_single(connection, inferencer, &single)?;
         buffer += &part;
     }
     Ok(buffer)
 }
 
-fn backwards_print_single(inferencer: &Inferencer, target: &Proposition) -> Result<String, Box<dyn Error>> {
+fn backwards_print_single(
+    connection: &mut Connection,
+    inferencer: &Inferencer,
+    target: &Proposition,
+) -> Result<String, Box<dyn Error>> {
     let proposition_node = PropositionNode::from_single(&target);
-    let backlinks = inferencer.proposition_graph.get_all_backward(&proposition_node);
+    let backlinks = inferencer
+        .proposition_graph
+        .get_all_backward(&proposition_node);
     let mut buffer = "".to_string();
     for backlink in &backlinks {
         let group = backlink.extract_group();
-        let part = backwards_print_group(inferencer, &group)?;
+        let part = backwards_print_group(connection, inferencer, &group)?;
         buffer += &part;
     }
-    buffer += &format!(r#"
+    buffer += &format!(
+        r#"
         <div class='network_cell'>
             {target_part}
         </div>
-    "#, target_part = diagram_predicate(&target.predicate));
-    let backimplications = extract_backimplications_from_proposition(&inferencer.model.graph, target).unwrap();
-    for backimplication in & backimplications {
-        buffer += &format!(r#"
+    "#,
+        target_part = diagram_predicate(&target.predicate)
+    );
+    let backimplications =
+        extract_backimplications_from_proposition(connection, &inferencer.model.graph, target)
+            .unwrap();
+    for backimplication in &backimplications {
+        buffer += &format!(
+            r#"
             <div class='network_cell'>
                 {implication_part}
             </div>
-        "#, implication_part = diagram_proposition_factor(backimplication));
+        "#,
+            implication_part = diagram_proposition_factor(backimplication)
+        );
     }
     Ok(buffer)
 }
@@ -48,14 +83,18 @@ fn backwards_print_single(inferencer: &Inferencer, target: &Proposition) -> Resu
 fn render_network(bundle: &ResourceContext, namespace: &str) -> Result<String, Box<dyn Error>> {
     let graph = InferenceGraph::new_shared(namespace.to_string())?;
     let mut connection = bundle.connection.lock().unwrap();
-    let target = graph.get_target(connection)?;
-    let proposition_graph = PropositionGraph::new_shared(&graph)?;
+    let target = graph.get_target(&mut connection)?;
+    let proposition_graph = PropositionGraph::new_shared(&mut connection, &graph, target)?;
     proposition_graph.visualize();
-    let model = InferenceModel::new_shared(namespace).unwrap();
+    let model = InferenceModel::new_shared(namespace.to_string()).unwrap();
     let fact_memory = EmptyBeliefTable::new_shared(namespace)?;
     let inferencer =
         Inferencer::new_mutable(model.clone(), proposition_graph.clone(), fact_memory)?;
-    let result = backwards_print_single(&inferencer, &inferencer.proposition_graph.target)?;
+    let result = backwards_print_single(
+        &mut connection,
+        &inferencer,
+        &inferencer.proposition_graph.target,
+    )?;
     Ok(result)
 }
 
