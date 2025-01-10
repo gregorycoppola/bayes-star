caee48d632dc94345ebd2303e52dea184473610c
Greg <greg@coppola.ai>
2025-01-10 07:36:02 -0500

    calling marginals from inside the handler!! woo!

--- rust/src/explorer/routes/marginals_route.rs
+++ rust/src/explorer/routes/marginals_route.rs
@@ -1,9 +1,11 @@
 use rocket::response::content::Html;
 
-use crate::{common::resources::ResourceContext, explorer::render_utils::render_app_body};
+use crate::{common::resources::ResourceContext, explorer::render_utils::render_app_body, inference::rounds::run_inference_rounds};
 
 
-pub fn internal_marginals(experiment_name: &str, test_scenario: &str, namespace: &ResourceContext) -> Html<String> {
+pub fn internal_marginals(experiment_name: &str, test_scenario: &str, resource_context: &ResourceContext) -> Html<String> {
+    let marginal_tables = run_inference_rounds(experiment_name, test_scenario, resource_context)
+        .expect("Testing failed.");
     let result = render_app_body("");
     Html(result.unwrap())
 }
