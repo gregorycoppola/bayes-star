bd54c79b17faabe41dbf677d4f04e79f06a03cbe
Greg <greg@coppola.ai>
2025-01-10 10:51:57 -0500

    make a stub for weights

--- rust/src/explorer/routes/weights_route.rs
+++ rust/src/explorer/routes/weights_route.rs
@@ -1,9 +1,28 @@
+use redis::Connection;
 use rocket::response::content::Html;
 
-use crate::{common::resources::ResourceContext, explorer::render_utils::render_app_body};
+use crate::{common::{graph::InferenceGraph, resources::ResourceContext}, explorer::{diagram_utils::diagram_implication, render_utils::render_app_body}};
+
+fn render_experiment_parts(connection: &mut Connection, graph: &InferenceGraph) -> String {
+    let mut buffer = format!(
+        r#"
+        <div class='section_header'>
+            Implication Factors
+        </div>
+    "#
+    );
+    let all_relations = graph.get_all_implications(connection).unwrap();
+    println!("all_relations {:?}", &all_relations);
+    for relation in &all_relations {
+        buffer += &diagram_implication(relation);
+    }
+    buffer
+}
 
 
-pub fn internal_weights(experiment_name: &str, namespace: &ResourceContext) -> Html<String> {
+pub fn internal_weights(experiment_name: &str, resources: &ResourceContext) -> Html<String> {
+    let mut connection = resources.connection.lock().unwrap();
+    let graph = InferenceGraph::new_mutable(experiment_name.to_string()).unwrap();
     let result = render_app_body("");
     Html(result.unwrap())
 }
