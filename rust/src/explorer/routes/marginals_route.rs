5714648b7ee4454daef374c417dd2131a1923fe8
Greg <greg@coppola.ai>
2025-01-10 07:46:41 -0500

    it's working.. but i think it's not really setting the variable..

--- rust/src/explorer/routes/marginals_route.rs
+++ rust/src/explorer/routes/marginals_route.rs
@@ -6,6 +6,14 @@ use crate::{common::resources::ResourceContext, explorer::render_utils::render_a
 pub fn internal_marginals(experiment_name: &str, test_scenario: &str, resource_context: &ResourceContext) -> Html<String> {
     let marginal_tables = run_inference_rounds(experiment_name, test_scenario, resource_context)
         .expect("Testing failed.");
-    let result = render_app_body("");
+
+    let mut body_html = "".to_string();
+    body_html += &format!("<div class='marginal_box'>");
+    for marginal_table in &marginal_tables {
+        let html_part = marginal_table.render_marginal_table();
+        body_html += &html_part;
+    }
+    body_html += &format!("</div>");
+    let result = render_app_body(&body_html);
     Html(result.unwrap())
 }
