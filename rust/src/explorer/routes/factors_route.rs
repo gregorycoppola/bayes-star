3221239c6ca335a3b5e555134828b830ab166614
Greg <greg@coppola.ai>
2025-01-10 18:00:13 -0500

    INFO [/Users/greg/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rocket-0.4.11/src/rocket.rs:741] 🚀 Rocket has launched from http://localhost:8000 INFO [/Users/greg/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rocket-0.4.11/src/rocket.rs:208] GET /factors/dating_simple text/html: INFO [/Users/greg/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rocket-0.4.11/src/rocket.rs:295] Matched: GET /factors/<experiment_name> (factors) thread '<unnamed>' panicked at src/inference/table.rs:85:18: This is not a single. stack backtrace:    0: rust_begin_unwind              at /rustc/aa067fb984d36462548bb785da221bfaf38253f0/library/std/src/panicking.rs:645:5    1: core::panicking::panic_fmt              at /rustc/aa067fb984d36462548bb785da221bfaf38253f0/library/core/src/panicking.rs:72:14    2: bayes_star::inference::table::PropositionNode::extract_single              at ./src/inference/table.rs:85:18    3: bayes_star::explorer::routes::factors_route::iterate_through_factors              at ./src/explorer/routes/factors_route.rs:28:27    4: bayes_star::explorer::routes::factors_route::internal_factors              at ./src/explorer/routes/factors_route.rs:36:21    5: explorer_server::factors              at ./src/bin/explorer_server.rs:54:5    6: explorer_server::rocket_route_fn_factors              at ./src/bin/explorer_server.rs:53:4    7: core::ops::function::Fn::call              at /rustc/aa067fb984d36462548bb785da221bfaf38253f0/library/core/src/ops/function.rs:79:5    8: <F as rocket::handler::Handler>::handle              at /Users/greg/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rocket-0.4.11/src/handler.rs:177:9    9: rocket::rocket::Rocket::route              at /Users/greg/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rocket-0.4.11/src/rocket.rs:299:27   10: rocket::rocket::Rocket::route_and_process

--- rust/src/explorer/routes/factors_route.rs
+++ rust/src/explorer/routes/factors_route.rs
@@ -31,26 +31,9 @@ fn iterate_through_factors(
     Ok(buffer)
 }
 
-fn render_implication_part(connection: &mut Connection, graph: &InferenceGraph) -> String {
-    let mut buffer = format!(
-        r#"
-        <div class='section_header'>
-            Implication Factors
-        </div>
-    "#
-    );
-    let all_relations = graph.get_all_implications(connection).unwrap();
-    println!("all_relations {:?}", &all_relations);
-    for relation in &all_relations {
-        buffer += &diagram_implication(relation);
-    }
-    buffer
-}
-
 pub fn internal_factors(experiment_name: &str, resource_context: &ResourceContext) -> Html<String> {
-    let mut connection = resource_context.connection.lock().unwrap();
     let graph = InferenceGraph::new_mutable(experiment_name.to_string()).unwrap();
-    let body_html = render_implication_part(&mut connection, &graph);
+    let body_html = iterate_through_factors(experiment_name, resource_context).unwrap();
     let result = render_app_body(&body_html);
     Html(result.unwrap())
 }
