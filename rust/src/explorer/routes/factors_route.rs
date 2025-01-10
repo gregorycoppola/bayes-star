523016c2ece46d503da40ee6000b3de384e7f6b0
Greg <greg@coppola.ai>
2025-01-10 08:37:23 -0500

    make a "factors" route stub

---
+++ rust/src/explorer/routes/factors_route.rs
@@ -0,0 +1,9 @@
+use rocket::response::content::Html;
+
+use crate::{common::resources::ResourceContext, explorer::render_utils::render_app_body};
+
+
+pub fn internal_factors(experiment_name: &str, namespace: &ResourceContext) -> Html<String> {
+    let result = render_app_body("");
+    Html(result.unwrap())
+}
