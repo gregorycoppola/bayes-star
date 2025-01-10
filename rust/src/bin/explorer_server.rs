afa00c776385971c70c6b87487234d96b53cc6f2
Greg <greg@coppola.ai>
2025-01-10 03:31:09 -0500

    web context

--- rust/src/bin/explorer_server.rs
+++ rust/src/bin/explorer_server.rs
@@ -13,41 +13,41 @@ use rocket::response::content::Html;
 use rocket::State;
 use rocket_contrib::serve::StaticFiles;
 
-pub struct AppContext {
+pub struct WebContext {
     namespace: ResourceContext,
 }
 
-impl AppContext {
+impl WebContext {
     pub fn new(config: CommandLineOptions) -> Self {
         let namespace = ResourceContext::new(&config).expect("Failed to create factory resources");
-        AppContext { namespace }
+        WebContext { namespace }
     }
 }
 
 #[get("/")]
-fn home(_context: State<AppContext>) -> Html<String> {
+fn home(_context: State<WebContext>) -> Html<String> {
     internal_index()
 }
 
 #[get("/experiment/<experiment_name>")]
-fn experiment(experiment_name: String, context: State<AppContext>) -> Html<String> {
+fn experiment(experiment_name: String, context: State<WebContext>) -> Html<String> {
     internal_experiment(&experiment_name, &context.namespace)
 }
 
 #[get("/network/<experiment_name>")]
-fn network(experiment_name: String, context: State<AppContext>) -> Html<String> {
+fn network(experiment_name: String, context: State<WebContext>) -> Html<String> {
     internal_network(&experiment_name, &context.namespace)
 }
 
 #[get("/weights/<experiment_name>")]
-fn weights(experiment_name: String, context: State<AppContext>) -> Html<String> {
+fn weights(experiment_name: String, context: State<WebContext>) -> Html<String> {
     internal_weights(&experiment_name, &context.namespace)
 }
 
 fn main() {
     let config = parse_configuration_options();
     rocket::ignite()
-        .manage(AppContext::new(config))
+        .manage(WebContext::new(config))
         .mount("/", routes![home, experiment, network, weights])
         .mount("/static", StaticFiles::from("static"))
         .launch();
