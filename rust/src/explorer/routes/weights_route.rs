54aa3616f868d79a95c48bdde59a316b573ccba9
Greg <greg@coppola.ai>
2025-01-10 16:31:27 -0500

    format

--- rust/src/explorer/routes/weights_route.rs
+++ rust/src/explorer/routes/weights_route.rs
@@ -1,14 +1,26 @@
 use redis::Connection;
 use rocket::response::content::Html;
 
-use crate::{common::{graph::InferenceGraph, resources::ResourceContext}, explorer::{diagram_utils::diagram_implication, render_utils::render_app_body}, model::{objects::ImplicationFactor, weights::{negative_feature, positive_feature, ExponentialWeights, CLASS_LABELS}}};
+use crate::{
+    common::{graph::InferenceGraph, resources::ResourceContext},
+    explorer::{diagram_utils::diagram_implication, render_utils::render_app_body},
+    model::{
+        objects::ImplicationFactor,
+        weights::{negative_feature, positive_feature, ExponentialWeights, CLASS_LABELS},
+    },
+};
 
-fn render_one_weight_box(connection: &mut Connection, graph: &InferenceGraph, factor:&ImplicationFactor) -> String {
+fn render_one_weight_box(
+    connection: &mut Connection,
+    graph: &InferenceGraph,
+    factor: &ImplicationFactor,
+) -> String {
     let weights = ExponentialWeights::new(graph.namespace.clone()).unwrap();
     let feature = factor.unique_key();
     let mut buffer = "".to_string();
     buffer += &format!("<div class='weight_box'>");
-    buffer += &format!(r#"
+    buffer += &format!(
+        r#"
         <div class='weight_box_row'>
             <div class='weight_box_cell'>
             </div>
@@ -19,7 +31,8 @@ fn render_one_weight_box(connection: &mut Connection, graph: &InferenceGraph, fa
                 true
             </div>
         </div>
-    "#);
+    "#
+    );
     for class_label in CLASS_LABELS {
         let posf = positive_feature(&feature, class_label);
         let negf = negative_feature(&feature, class_label);
@@ -27,19 +40,20 @@ fn render_one_weight_box(connection: &mut Connection, graph: &InferenceGraph, fa
         let negf_count = weights.read_single_weight(connection, &negf).unwrap();
         let posf_css = if posf_count > 0.1f64 {
             "positive_weight".to_string()
-        } else if posf_count <  -0.1f64 {
+        } else if posf_count < -0.1f64 {
             "negative_weight".to_string()
         } else {
             "neutral_weight".to_string()
         };
         let negf_css = if negf_count > 0.1f64 {
             "positive_weight".to_string()
-        } else if negf_count <  -0.1f64 {
+        } else if negf_count < -0.1f64 {
             "negative_weight".to_string()
         } else {
             "neutral_weight".to_string()
         };
-        buffer += &format!(r#"
+        buffer += &format!(
+            r#"
             <div class='weight_box_row'>
                 <div class='weight_box_cell'>
                     {class_label}
@@ -51,7 +65,8 @@ fn render_one_weight_box(connection: &mut Connection, graph: &InferenceGraph, fa
                     {posf_count}
                 </div>
             </div>
-        "#);
+        "#
+        );
     }
     buffer += &format!("</div>");
     buffer
@@ -73,7 +88,6 @@ fn render_weights_part(connection: &mut Connection, graph: &InferenceGraph) -> S
     }
     buffer
 }
-
 
 pub fn internal_weights(experiment_name: &str, resources: &ResourceContext) -> Html<String> {
     let mut connection = resources.connection.lock().unwrap();
