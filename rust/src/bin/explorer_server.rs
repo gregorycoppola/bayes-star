94a692f01ed192364d8abfcc38edd74223a05fd6
Greg <greg@coppola.ai>
2025-01-11 10:40:28 -0500

    hook up the "animation" part
    just need to impelement

--- rust/src/bin/explorer_server.rs
+++ rust/src/bin/explorer_server.rs
@@ -7,7 +7,7 @@ use bayes_star::{
         resources::ResourceContext,
         setup::{parse_configuration_options, CommandLineOptions},
     },
-    explorer::routes::{experiment_route::internal_experiment, factors_route::internal_factors, index_route::internal_index, marginals_route::internal_marginals, network_route::internal_network, weights_route::internal_weights},
+    explorer::routes::{animation_route::internal_animation, experiment_route::internal_experiment, factors_route::internal_factors, index_route::internal_index, marginals_route::internal_marginals, network_route::internal_network, weights_route::internal_weights},
 };
 use rocket::response::content::Html;
 use rocket::State;
@@ -54,11 +54,16 @@ fn factors(experiment_name: String, context: State<WebContext>) -> Html<String> 
     internal_factors(&experiment_name, &context.namespace)
 }
 
+#[get("/animation/<experiment_name>")]
+fn animation(experiment_name: String, context: State<WebContext>) -> Html<String> {
+    internal_animation(&experiment_name, &context.namespace)
+}
+
 fn main() {
     let config = parse_configuration_options();
     rocket::ignite()
         .manage(WebContext::new(config))
-        .mount("/", routes![home, experiment, network, weights, marginals, factors])
+        .mount("/", routes![home, experiment, network, weights, marginals, factors, animation])
         .mount("/static", StaticFiles::from("static"))
         .launch();
 }
