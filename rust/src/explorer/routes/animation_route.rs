c5769c881053501040633fa1cf22c7e2ab689778
Greg <greg@coppola.ai>
2025-01-13 05:32:28 -0500

    hack in the marginal table

--- rust/src/explorer/routes/animation_route.rs
+++ rust/src/explorer/routes/animation_route.rs
@@ -3,7 +3,26 @@ use std::error::Error;
 use redis::Connection;
 use rocket::response::content::Html;
 
-use crate::{common::{graph::InferenceGraph, model::InferenceModel, proposition_db::EmptyBeliefTable, resources::ResourceContext}, explorer::{diagram_utils::{diagram_predicate, diagram_proposition_factor}, render_utils::render_app_body}, inference::{graph::PropositionGraph, inference::Inferencer, rounds::run_inference_rounds, table::PropositionNode}, model::{choose::extract_backimplications_from_proposition, objects::{Proposition, PropositionGroup}}};
+use crate::{
+    common::{
+        graph::InferenceGraph, model::InferenceModel, proposition_db::EmptyBeliefTable,
+        resources::ResourceContext,
+    },
+    explorer::{
+        diagram_utils::{diagram_predicate, diagram_proposition_factor},
+        render_utils::render_app_body,
+    },
+    inference::{
+        graph::PropositionGraph,
+        inference::{Inferencer, MarginalTable},
+        rounds::run_inference_rounds,
+        table::PropositionNode,
+    },
+    model::{
+        choose::extract_backimplications_from_proposition,
+        objects::{Proposition, PropositionGroup},
+    },
+};
 
 fn backwards_print_group(
     connection: &mut Connection,
@@ -33,18 +52,18 @@ fn backwards_print_single(
         .proposition_graph
         .get_all_backward(&proposition_node);
     let mut buffer = "".to_string();
-    buffer += &format!( r#" <div class='proof_box'> "#,);
-    buffer += &format!( r#" <div class='network_row'> "#,);
+    buffer += &format!(r#" <div class='proof_box'> "#,);
+    buffer += &format!(r#" <div class='network_row'> "#,);
     for backlink in &backlinks {
         let group = backlink.extract_group();
         let part = backwards_print_group(connection, inferencer, &group)?;
         buffer += &part;
     }
-    buffer += &format!( r#" </div>"#,);
+    buffer += &format!(r#" </div>"#,);
     let backimplications =
         extract_backimplications_from_proposition(connection, &inferencer.model.graph, target)
             .unwrap();
-    buffer += &format!( r#" <div class='network_row'> "#,);
+    buffer += &format!(r#" <div class='network_row'> "#,);
     for backimplication in &backimplications {
         buffer += &format!(
             r#"
@@ -55,7 +74,7 @@ fn backwards_print_single(
             implication_part = diagram_proposition_factor(backimplication)
         );
     }
-    buffer += &format!( r#" </div> "#,);
+    buffer += &format!(r#" </div> "#,);
     buffer += &format!(
         r#"
         <div class='network_row'>
@@ -64,11 +83,15 @@ fn backwards_print_single(
     "#,
         target_part = diagram_predicate(&target.predicate)
     );
-    buffer += &format!( r#" </div> "#,); // "proof_box"
+    buffer += &format!(r#" </div> "#,); // "proof_box"
     Ok(buffer)
 }
 
-fn safe_network_animations(connection: &mut Connection, namespace: &str, ) -> Result<String, Box<dyn Error>> {
+fn safe_network_animations(
+    connection: &mut Connection,
+    namespace: &str,
+    marginal_tables: &Vec<MarginalTable>,
+) -> Result<String, Box<dyn Error>> {
     let graph = InferenceGraph::new_shared(namespace.to_string())?;
     let target = graph.get_target(connection)?;
     let proposition_graph = PropositionGraph::new_shared(connection, &graph, target)?;
@@ -93,7 +116,7 @@ pub fn internal_animation(
     let mut connection = resource_context.connection.lock().unwrap();
     let marginal_tables = run_inference_rounds(&mut connection, experiment_name, test_scenario)
         .expect("Testing failed.");
-    let body_html = safe_network_animations(&mut connection, experiment_name).unwrap();
+    let body_html = safe_network_animations(&mut connection, experiment_name, &marginal_tables).unwrap();
     let result = render_app_body(&body_html);
     Html(result.unwrap())
 }
